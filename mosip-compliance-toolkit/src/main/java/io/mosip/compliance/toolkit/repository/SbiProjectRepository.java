794e96b2e4dd21ad5645be0ff8f5d0a2f389196b
srinivas-mosip <srinivas.mosip@gmail.com>
2022-07-25 16:10:23 +0530

    repository changes for getSbiProject

--- mosip-compliance-toolkit/src/main/java/io/mosip/compliance/toolkit/repository/SbiProjectRepository.java
+++ mosip-compliance-toolkit/src/main/java/io/mosip/compliance/toolkit/repository/SbiProjectRepository.java
@@ -1,13 +1,13 @@
 package io.mosip.compliance.toolkit.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.transaction.Transactional;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import io.mosip.compliance.toolkit.entity.ProjectSummaryEntity;
 import io.mosip.compliance.toolkit.entity.SbiProjectEntity;
 import io.mosip.kernel.core.dataaccess.spi.repository.BaseRepository;
 
@@ -24,5 +24,8 @@ public interface SbiProjectRepository extends BaseRepository<SbiProjectEntity, S
 
 	@Query("SELECT e FROM SbiProjectEntity e  WHERE e.partnerId= ?1 and e.projectType='SBI' and e.isDeleted<>'true' order by e.crDate desc")
 	public List<SbiProjectEntity> findAllByPartnerId(String partnerId);
+
+	public Optional<SbiProjectEntity> findById(String id);
+	
 	
 }
