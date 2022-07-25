4d1c7947a4813190b9d4f25d90133fcf676f9ded
srinivas-mosip <srinivas.mosip@gmail.com>
2022-07-25 15:45:32 +0530

    Sbi project controller, service, response dto added, getProjectById request added

---
+++ mosip-compliance-toolkit/src/main/java/io/mosip/compliance/toolkit/controllers/SbiProjectController.java
@@ -0,0 +1,39 @@
+package io.mosip.compliance.toolkit.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.mosip.compliance.toolkit.dto.SbiProjectResponseDto;
+import io.mosip.compliance.toolkit.service.SbiProjectService;
+import io.mosip.compliance.toolkit.util.RequestValidator;
+import io.mosip.kernel.core.http.ResponseWrapper;
+
+@RestController
+public class SbiProjectController {
+	
+	@Autowired
+	private SbiProjectService sbiProjectService;
+	
+	@Autowired
+	private RequestValidator requestValidator;
+	
+	/**
+	 * Initiates the binder.
+	 *
+	 * @param binder the binder
+	 */
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		binder.addValidators(requestValidator);
+	}
+	
+	@GetMapping(value = "/getSbiProject")
+	private ResponseWrapper<SbiProjectResponseDto> getProjectById(@RequestParam(required = true) String id){
+		return sbiProjectService.getSbiProject(id);
+	}
+
+}
