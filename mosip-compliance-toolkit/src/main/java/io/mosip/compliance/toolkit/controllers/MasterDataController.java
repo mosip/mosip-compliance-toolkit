b671df1e530b579c4641bfb476430e09a47aa68f
srinivas-mosip <srinivas.mosip@gmail.com>
2022-07-25 16:41:41 +0530

    master data changes

--- mosip-compliance-toolkit/src/main/java/io/mosip/compliance/toolkit/controllers/MasterDataController.java
+++ mosip-compliance-toolkit/src/main/java/io/mosip/compliance/toolkit/controllers/MasterDataController.java
@@ -1,19 +1,18 @@
 package io.mosip.compliance.toolkit.controllers;
 
-import java.util.Base64;
-import java.util.Map;
-
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.mosip.compliance.toolkit.dto.MasterDataDto;
 import io.mosip.compliance.toolkit.service.MasterDataService;
+import io.mosip.compliance.toolkit.util.RequestValidator;
+import io.mosip.kernel.core.http.RequestWrapper;
 import io.mosip.kernel.core.http.ResponseWrapper;
 
 @RestController
@@ -21,22 +20,30 @@ public class MasterDataController {
 	
 	@Autowired
 	private MasterDataService masterDataService;
+	
+	@Autowired
+	private RequestValidator requestValidator;
 
-	@GetMapping(value = "/masterdata") 
-	public ResponseWrapper<MasterDataDto> getMasterDataByName(@RequestParam String name){
+	/**
+	 * Initiates the binder.
+	 *
+	 * @param binder the binder
+	 */
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		binder.addValidators(requestValidator);
+	}
+
+	@GetMapping(value = "/getMasterData") 
+	public ResponseWrapper<MasterDataDto> getMasterDataByName(@RequestParam(required = true) String name){
 		return masterDataService.getDataByName(name); 
 	}
 
-	@PostMapping(value = "/masterdata")
-	public ResponseWrapper<MasterDataDto> addMasterData(@RequestHeader Map<String, String> headers,@RequestBody MasterDataDto masterDataDto) {
-		//String token = headers.get("cookie");
-		//return masterDataService.addMasterData(getEmail(token.split("Authorization=")[1]), masterDataDto);
-		return masterDataService.addMasterData("admin", masterDataDto);
+	@PostMapping(value = "/saveMasterData")
+	public ResponseWrapper<MasterDataDto> addMasterData(
+			@RequestBody(required = true) RequestWrapper<MasterDataDto> masterDataDtoRequest) {
+		MasterDataDto masterDataDto = masterDataDtoRequest.getRequest();
+		return masterDataService.addMasterData(masterDataDto);
 	}
 	
-//	private String getEmail(String token) {
-//		String[] jwtPart = token.split("\\.");
-//		JSONObject payloadJsonObj = new JSONObject(new String(Base64.getUrlDecoder().decode(jwtPart[1])));
-//		return payloadJsonObj.getString("email");
-//	}
 }
