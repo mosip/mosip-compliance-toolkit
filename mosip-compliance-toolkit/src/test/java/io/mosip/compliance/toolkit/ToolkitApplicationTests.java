d48463ce1a43ac90c5b967473e6b1694cc4c79fb
srinivas-mosip <srinivas.mosip@gmail.com>
2022-07-26 14:42:49 +0530

    MOSIP-23191: ToolkitTestCases file added

---
+++ mosip-compliance-toolkit/src/test/java/io/mosip/compliance/toolkit/ToolkitApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mosip.compliance.toolkit;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ToolkitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
