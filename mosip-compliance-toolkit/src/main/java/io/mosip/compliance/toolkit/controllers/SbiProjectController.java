f41512529398bcf2cccb0dfa46ae517d418dd2cc
srinivas-mosip <srinivas.mosip@gmail.com>
2022-07-27 10:07:14 +0530

    MOSIP-23191:Organised imports in file

--- mosip-compliance-toolkit/src/main/java/io/mosip/compliance/toolkit/controllers/SbiProjectController.java
+++ mosip-compliance-toolkit/src/main/java/io/mosip/compliance/toolkit/controllers/SbiProjectController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.mosip.compliance.toolkit.dto.SbiProjectDto;
