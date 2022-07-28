57e6eca3531586536651b73b47ab68529388531a
JanardhanBS-SyncByte <janardhan@syncbyte.in>
2022-07-28 13:14:55 +0530

    MOSIP-21500

--- mosip-compliance-toolkit/src/main/java/io/mosip/compliance/toolkit/controllers/SbiProjectController.java
+++ mosip-compliance-toolkit/src/main/java/io/mosip/compliance/toolkit/controllers/SbiProjectController.java
@@ -1,19 +1,31 @@
 package io.mosip.compliance.toolkit.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.mosip.compliance.toolkit.dto.SbiProjectDto;
 import io.mosip.compliance.toolkit.service.SbiProjectService;
+import io.mosip.compliance.toolkit.util.DataValidationUtil;
 import io.mosip.compliance.toolkit.util.RequestValidator;
+import io.mosip.kernel.core.http.RequestWrapper;
+import io.mosip.kernel.core.http.ResponseFilter;
 import io.mosip.kernel.core.http.ResponseWrapper;
 
 @RestController
 public class SbiProjectController {
+	
+	/** CREATE The Constant  SBI_PROJECT_POST in application as given below */
+	//mosip.toolkit.api.id.sbi.project.post=mosip.toolkit.sbi.project.add
+	private static final String SBI_PROJECT_POST = "sbi.project.post";
 	
 	@Autowired
 	private SbiProjectService sbiProjectService;
@@ -36,4 +48,21 @@ public class SbiProjectController {
 		return sbiProjectService.getSbiProject(id);
 	}
 
+	/**
+	* Post Sbi Project details.
+	*
+	* @param SbiProjectDto
+	* @return SbiProjectDto added
+	 * @throws Exception 
+	*/
+	@ResponseFilter
+	@PostMapping(value = "/addSbiProject", produces = "application/json")
+	public ResponseWrapper<SbiProjectDto> addSbiProject(
+			@RequestBody @Valid RequestWrapper<SbiProjectDto> value,
+			Errors errors) throws Exception{
+		requestValidator.validate(value, errors);
+		requestValidator.validateId(SBI_PROJECT_POST, value.getId(), errors);
+		DataValidationUtil.validate(errors, SBI_PROJECT_POST);
+		return sbiProjectService.addSbiProject(value.getRequest());
+	}
 }
--- mosip-compliance-toolkit/src/main/java/io/mosip/compliance/toolkit/service/SbiProjectService.java
+++ mosip-compliance-toolkit/src/main/java/io/mosip/compliance/toolkit/service/SbiProjectService.java
@@ -16,10 +16,17 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import io.mosip.compliance.toolkit.config.LoggerConfiguration;
 import io.mosip.compliance.toolkit.constants.AppConstants;
+import io.mosip.compliance.toolkit.constants.DeviceSubTypes;
+import io.mosip.compliance.toolkit.constants.DeviceTypes;
+import io.mosip.compliance.toolkit.constants.ProjectTypes;
+import io.mosip.compliance.toolkit.constants.Purposes;
+import io.mosip.compliance.toolkit.constants.SpecVersions;
 import io.mosip.compliance.toolkit.constants.ToolkitErrorCodes;
 import io.mosip.compliance.toolkit.dto.SbiProjectDto;
 import io.mosip.compliance.toolkit.entity.SbiProjectEntity;
+import io.mosip.compliance.toolkit.exceptions.ToolkitException;
 import io.mosip.compliance.toolkit.repository.SbiProjectRepository;
+import io.mosip.compliance.toolkit.util.RandomIdGenerator;
 import io.mosip.kernel.core.authmanager.authadapter.model.AuthUserDetails;
 import io.mosip.kernel.core.exception.ServiceError;
 import io.mosip.kernel.core.http.ResponseWrapper;
@@ -32,16 +39,20 @@ public class SbiProjectService {
 	@Autowired
 	private SbiProjectRepository sbiProjectRepository;
 	private Logger log = LoggerConfiguration.logConfig(SbiProjectService.class);
-
 	
 	private AuthUserDetails authUserDetails() {
 		return (AuthUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 	}
+	
 	private String getPartnerId() {
 		String partnerId = authUserDetails().getUsername();
 		return partnerId;
 	}
-	
+		
+	private String getUserBy() {
+		String crBy = authUserDetails().getMail();
+		return crBy;
+	}
 	
 	public ResponseWrapper<SbiProjectDto> getSbiProject(String id) {
 		ResponseWrapper<SbiProjectDto> responseWrapper = new ResponseWrapper<>();
@@ -81,4 +92,212 @@ public class SbiProjectService {
 		return responseWrapper;
 	}
 
+	public ResponseWrapper<SbiProjectDto> addSbiProject(SbiProjectDto sbiProjectDto) {
+		ResponseWrapper<SbiProjectDto> responseWrapper = new ResponseWrapper<>();
+
+		try
+		{
+			if (isValidSbiProject(sbiProjectDto))
+			{
+				LocalDateTime crDate = LocalDateTime.now();
+				SbiProjectEntity entity = new SbiProjectEntity ();
+				entity.setId(RandomIdGenerator.generateUUID(sbiProjectDto.getProjectType().toLowerCase(), "", 36));
+				entity.setName(sbiProjectDto.getName());
+				entity.setProjectType(sbiProjectDto.getProjectType());
+				entity.setSbiVersion(sbiProjectDto.getSbiVersion());
+				entity.setPurpose(sbiProjectDto.getPurpose());
+				entity.setDeviceType(sbiProjectDto.getDeviceType());
+				entity.setDeviceSubType(sbiProjectDto.getDeviceSubType());
+				entity.setPartnerId(this.getPartnerId());
+				entity.setCrBy(this.getUserBy());
+				entity.setCrDate(crDate);
+				entity.setDeleted(false);
+
+				sbiProjectRepository.save(entity);
+
+				sbiProjectDto.setId(entity.getId());
+				sbiProjectDto.setPartnerId(entity.getPartnerId());
+				sbiProjectDto.setCrBy(entity.getCrBy());
+				sbiProjectDto.setCrDate(entity.getCrDate());
+			}
+		}catch (ToolkitException ex) {
+			log.debug("sessionId", "idType", "id", ex.getStackTrace());
+			log.error("sessionId", "idType", "id", "In getSbiProject method of SbiProjectService Service - " + ex.getMessage());
+			List<ServiceError> serviceErrorsList = new ArrayList<>();
+			ServiceError serviceError = new ServiceError();
+			serviceError.setErrorCode(ex.getErrorCode());
+			serviceError.setMessage(ex.getMessage());
+			serviceErrorsList.add(serviceError);
+			responseWrapper.setErrors(serviceErrorsList);
+		}
+		catch (Exception ex) {
+			log.debug("sessionId", "idType", "id", ex.getStackTrace());
+			log.error("sessionId", "idType", "id", "In getSbiProject method of SbiProjectService Service - " + ex.getMessage());
+			List<ServiceError> serviceErrorsList = new ArrayList<>();
+			ServiceError serviceError = new ServiceError();
+			serviceError.setErrorCode(ToolkitErrorCodes.SBI_PROJECT_UNABLE_TO_ADD.getErrorCode());
+			serviceError.setMessage(ToolkitErrorCodes.SBI_PROJECT_UNABLE_TO_ADD.getErrorMessage()+ " " + ex.getMessage());
+			serviceErrorsList.add(serviceError);
+			responseWrapper.setErrors(serviceErrorsList);
+		}
+		responseWrapper.setId(getSbiProjectId);
+		responseWrapper.setResponse(sbiProjectDto);
+		responseWrapper.setVersion(AppConstants.VERSION);		
+		responseWrapper.setResponsetime(LocalDateTime.now());
+		return responseWrapper;
+	}
+
+	/**
+	* Verifies all values of SbiProjectDto.
+	* validates ProjectType, SpecVersion, Purpose, DeviceType, DeviceSubType
+	*
+	* @param SbiProjectDto
+	* @return boolean
+	*/
+	private boolean isValidSbiProject(SbiProjectDto sbiProjectDto) throws ToolkitException {
+		ToolkitErrorCodes errorCode = null; 
+
+		ProjectTypes projectTypesCode = ProjectTypes.fromCode(sbiProjectDto.getProjectType());
+		SpecVersions specVersionCode = SpecVersions.fromCode(sbiProjectDto.getSbiVersion());
+		Purposes purposeCode = Purposes.fromCode(sbiProjectDto.getPurpose());
+		DeviceTypes deviceTypeCode = DeviceTypes.fromCode(sbiProjectDto.getDeviceType());
+		DeviceSubTypes deviceSubTypeCode = DeviceSubTypes.fromCode(sbiProjectDto.getDeviceSubType());
+
+		switch(projectTypesCode)
+		{
+			case SBI:
+				switch(specVersionCode)
+				{
+					case SPEC_VER_0_9_5:
+					case SPEC_VER_1_0_0:
+						break;
+					default:
+						errorCode = ToolkitErrorCodes.INVALID_SPEC_VERSION_FOR_PROJECT_TYPE_;
+						throw new ToolkitException (errorCode.getErrorCode(), errorCode.getErrorMessage ());
+				}
+				break;
+			default:
+				errorCode = ToolkitErrorCodes.INVALID_PROJECT_TYPE;
+				throw new ToolkitException (errorCode.getErrorCode(), errorCode.getErrorMessage ());
+		}
+
+		switch(purposeCode)
+		{
+			case AUTH:
+				switch(deviceTypeCode)
+				{
+					case FINGER:
+						switch(deviceSubTypeCode)
+						{
+							case SINGLE:
+							case TOUCHLESS:
+							case SLAP:
+								break;
+							case DOUBLE:
+							case FULL_FACE:
+								errorCode = ToolkitErrorCodes.INVALID_DEVICE_SUB_TYPE_FOR_DEVICE_TYPE;
+								throw new ToolkitException (errorCode.getErrorCode(), errorCode.getErrorMessage ());
+							default:
+								errorCode = ToolkitErrorCodes.INVALID_DEVICE_SUB_TYPE;
+								throw new ToolkitException (errorCode.getErrorCode(), errorCode.getErrorMessage ());
+						}
+						break;
+					case IRIS:
+						switch(deviceSubTypeCode)
+						{
+							case SINGLE:
+							case DOUBLE:
+								break;
+							case SLAP:
+							case TOUCHLESS:
+							case FULL_FACE:
+								errorCode = ToolkitErrorCodes.INVALID_DEVICE_SUB_TYPE_FOR_DEVICE_TYPE;
+								throw new ToolkitException (errorCode.getErrorCode(), errorCode.getErrorMessage ());
+							default:
+								errorCode = ToolkitErrorCodes.INVALID_DEVICE_SUB_TYPE;
+								throw new ToolkitException (errorCode.getErrorCode(), errorCode.getErrorMessage ());
+						}
+						break;
+					case FACE:
+						switch(deviceSubTypeCode)
+						{
+							case FULL_FACE:
+								break;
+							case SINGLE:
+							case DOUBLE:
+							case SLAP:
+							case TOUCHLESS:
+								errorCode = ToolkitErrorCodes.INVALID_DEVICE_SUB_TYPE_FOR_DEVICE_TYPE;
+								throw new ToolkitException (errorCode.getErrorCode(), errorCode.getErrorMessage ());
+							default:
+								errorCode = ToolkitErrorCodes.INVALID_DEVICE_SUB_TYPE;
+								throw new ToolkitException (errorCode.getErrorCode(), errorCode.getErrorMessage ());
+						}
+						break;
+					default:
+						errorCode = ToolkitErrorCodes.INVALID_DEVICE_TYPE;
+						throw new ToolkitException (errorCode.getErrorCode(), errorCode.getErrorMessage ());
+				}
+				break;
+			case REGISTRATION:
+				switch(deviceTypeCode)
+				{
+					case FINGER:				
+						switch(deviceSubTypeCode)
+						{
+							case SLAP:				
+							case TOUCHLESS:
+								break;
+							case SINGLE:
+							case DOUBLE:
+							case FULL_FACE:
+								errorCode = ToolkitErrorCodes.INVALID_DEVICE_SUB_TYPE_FOR_DEVICE_TYPE;
+								throw new ToolkitException (errorCode.getErrorCode(), errorCode.getErrorMessage ());
+							default:
+								errorCode = ToolkitErrorCodes.INVALID_DEVICE_SUB_TYPE;
+								throw new ToolkitException (errorCode.getErrorCode(), errorCode.getErrorMessage ());
+						}
+						break;
+					case IRIS:
+						switch(deviceSubTypeCode)
+						{
+							case DOUBLE:
+								break;
+							case SINGLE:
+							case TOUCHLESS:
+							case FULL_FACE:
+								errorCode = ToolkitErrorCodes.INVALID_DEVICE_SUB_TYPE_FOR_DEVICE_TYPE;
+								throw new ToolkitException (errorCode.getErrorCode(), errorCode.getErrorMessage ());
+							default:
+								errorCode = ToolkitErrorCodes.INVALID_DEVICE_SUB_TYPE;
+								throw new ToolkitException (errorCode.getErrorCode(), errorCode.getErrorMessage ());
+						}
+						break;
+					case FACE:
+						switch(deviceSubTypeCode)
+						{
+							case FULL_FACE:
+								break;
+							case SINGLE:
+							case DOUBLE:
+							case TOUCHLESS:
+								errorCode = ToolkitErrorCodes.INVALID_DEVICE_SUB_TYPE_FOR_DEVICE_TYPE;
+								throw new ToolkitException (errorCode.getErrorCode(), errorCode.getErrorMessage ());
+							default:
+								errorCode = ToolkitErrorCodes.INVALID_DEVICE_SUB_TYPE;
+								throw new ToolkitException (errorCode.getErrorCode(), errorCode.getErrorMessage ());
+						}
+						break;
+					default:
+						errorCode = ToolkitErrorCodes.INVALID_DEVICE_TYPE;
+						throw new ToolkitException (errorCode.getErrorCode(), errorCode.getErrorMessage ());
+				}
+				break;
+			default:
+				errorCode = ToolkitErrorCodes.INVALID_PURPOSE;
+				throw new ToolkitException (errorCode.getErrorCode(), errorCode.getErrorMessage ());
+		}
+
+		return true;
+	}
 }
