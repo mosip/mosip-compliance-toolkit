327294f657fcffabd77c593484962ec1e86047b8
Mayura Deshmukh <mayura.deshmukh@gmail.com>
2022-08-02 18:35:11 +0530

    reverted ObjectMapperConfig due to Date issue

--- mosip-compliance-toolkit/src/main/java/io/mosip/compliance/toolkit/util/ObjectMapperConfig.java
+++ mosip-compliance-toolkit/src/main/java/io/mosip/compliance/toolkit/util/ObjectMapperConfig.java
@@ -8,10 +8,8 @@ import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
-@Configuration
 public class ObjectMapperConfig {
-	@Bean
-    public ObjectMapper objectMapper() {
+	public ObjectMapper objectMapper() {
         ObjectMapper mapper = new ObjectMapper();
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         mapper.configure(MapperFeature.DEFAULT_VIEW_INCLUSION, true);
