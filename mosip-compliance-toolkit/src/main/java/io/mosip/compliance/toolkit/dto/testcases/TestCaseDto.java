d82780f7459f91b22060e0fcb393cbab4867cd3d
Mayura Deshmukh <mayura.deshmukh@gmail.com>
2022-08-23 16:07:33 +0530

    Fixed testcase schema validation issue

--- mosip-compliance-toolkit/src/main/java/io/mosip/compliance/toolkit/dto/testcases/TestCaseDto.java
+++ mosip-compliance-toolkit/src/main/java/io/mosip/compliance/toolkit/dto/testcases/TestCaseDto.java
@@ -2,9 +2,9 @@ package io.mosip.compliance.toolkit.dto.testcases;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
-
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,42 +12,42 @@ import lombok.Setter;
 @Data
 @Getter
 @Setter
+@JsonInclude(Include.NON_NULL)
 public class TestCaseDto implements Serializable {
 	private static final long serialVersionUID = -679461384553195804L;
-
 	public String testCaseType;
-    public String testId;
-    public String specVersion;
-    public String testName;
-    public String testDescription;
-    @JsonProperty("isNegativeTestcase") 
-    public boolean isNegativeTestcase;
-    public String methodName;
-    public Object requestSchema;
-    public String responseSchema;
-    public ArrayList<ValidatorDef> validatorDefs;
-    public OtherAttributes otherAttributes;
-    
-    @Getter
-    @Setter
-    public static class ValidatorDef{
-        public String name;
-        public String description;
-    }
+	public String testId;
+	public String specVersion;
+	public String testName;
+	public String testDescription;
+	@JsonProperty("isNegativeTestcase")
+	public boolean isNegativeTestcase;
+	public String methodName;
+	public Object requestSchema;
+	public String responseSchema;
+	public ArrayList<ValidatorDef> validatorDefs;
+	public OtherAttributes otherAttributes;
 
-    @Getter
-    @Setter
-    public static class OtherAttributes{
-        public Object runtimeInput;
-        public ArrayList<Object> purpose;
-        public ArrayList<Object> biometricTypes;
-        public ArrayList<Object> deviceSubTypes;
-        public ArrayList<Object> segments;
-        public ArrayList<Object> exceptions;
-        public Object requestedScore;
-        public String bioCount;
-        public String deviceSubId;
-        public ArrayList<String> modalities;
-    }
+	@Getter
+	@Setter
+	public static class ValidatorDef {
+		public String name;
+		public String description;
+	}
+
+	@Getter
+	@Setter
+	@JsonInclude(Include.NON_NULL)
+	public static class OtherAttributes {
+		public Object runtimeInput;
+		public ArrayList<Object> purpose;
+		public ArrayList<Object> biometricTypes;
+		public ArrayList<Object> deviceSubTypes;
+		public ArrayList<Object> segments;
+		public ArrayList<Object> exceptions;
+		public Object requestedScore;
+		public String bioCount;
+		public String deviceSubId;
+		public ArrayList<String> modalities;
+	}
 }
-
