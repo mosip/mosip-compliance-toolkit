49ae21dc7922fd5fd93a3623b7f00698a8c62fb7
srinivas-mosip <srinivas.mosip@gmail.com>
2022-08-25 16:05:11 +0530

    MOSIP-21502: GET endpoint, biometric testdata

---
+++ mosip-compliance-toolkit/src/main/java/io/mosip/compliance/toolkit/entity/BiometricTestDataEntity.java
@@ -0,0 +1,67 @@
+package io.mosip.compliance.toolkit.entity;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Component
+@Entity
+@Table(name = "biometric_testdata", schema = "toolkit")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class BiometricTestDataEntity implements Serializable {
+	/**
+	* 
+	*/
+	private static final long serialVersionUID = -1163852475507127582L;
+
+	@Id
+	private String id;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "type")
+	private String type;
+
+	@Column(name = "purpose")
+	private String purpose;
+
+	@Column(name = "partner_id")
+	private String partnerId;
+
+	@Column(name = "file_id")
+	private String fileId;
+
+	@Column(name = "cr_by")
+	private String crBy;
+
+	@Column(name = "cr_dtimes")
+	private LocalDateTime crDate;
+
+	@Column(name = "upd_by")
+	private String upBy;
+
+	@Column(name = "upd_dtimes")
+	private LocalDateTime updDate;
+
+	@Column(name = "is_deleted")
+	private boolean isDeleted;
+
+	@Column(name = "del_dtimes")
+	private LocalDateTime delTime;
+
+}
