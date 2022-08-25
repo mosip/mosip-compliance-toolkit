7175aa1411b30d0505822693840f3d6d7b91e342
vipuldhurve02 <vipul.dhurve@technoforte.co.in>
2022-08-25 13:27:28 +0530

    [MOSIP-23617] api services : Get list of testcases for a sdk project by sdkPurpose

--- mosip-compliance-toolkit/src/main/java/io/mosip/compliance/toolkit/controllers/TestCasesController.java
+++ mosip-compliance-toolkit/src/main/java/io/mosip/compliance/toolkit/controllers/TestCasesController.java
@@ -36,111 +36,113 @@ import io.mosip.kernel.core.http.ResponseWrapper;
 
 /**
  * Controller for all testcases related end points.
- * 
+ *
  * @author Mayura D
  * @since 1.0.0
  */
 @RestController
 public class TestCasesController {
 
-	/** The Constant VALIDATIONS_POST_ID application. */
-	private static final String VALIDATIONS_POST_ID = "validations.post";
+    /**
+     * The Constant VALIDATIONS_POST_ID application.
+     */
+    private static final String VALIDATIONS_POST_ID = "validations.post";
 
-	@Value("${mosip.toolkit.api.id.testcase.project.get}")
-	private String getTestCasesId;
+    @Value("${mosip.toolkit.api.id.testcase.project.get}")
+    private String getTestCasesId;
 
-	@Autowired
-	TestCasesService service;
+    @Autowired
+    TestCasesService service;
 
-	@Autowired
-	private RequestValidator requestValidator;
+    @Autowired
+    private RequestValidator requestValidator;
 
-	/**
-	 * Initiates the binder.
-	 *
-	 * @param binder the binder
-	 */
-	@InitBinder
-	public void initBinder(WebDataBinder binder) {
-		binder.addValidators(requestValidator);
-	}
+    /**
+     * Initiates the binder.
+     *
+     * @param binder the binder
+     */
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        binder.addValidators(requestValidator);
+    }
 
-	@PostMapping(value = "/validateRequest")
-	public ResponseWrapper<ValidationResultDto> validateRequest(
-			@RequestBody @Valid RequestWrapper<ValidateRequestSchemaDto> input, Errors errors) throws Exception {
-		requestValidator.validateId(VALIDATIONS_POST_ID, input.getId(), errors);
-		DataValidationUtil.validate(errors, VALIDATIONS_POST_ID);
-		return service.performRequestValidations(input.getRequest());
-	}
+    @PostMapping(value = "/validateRequest")
+    public ResponseWrapper<ValidationResultDto> validateRequest(
+            @RequestBody @Valid RequestWrapper<ValidateRequestSchemaDto> input, Errors errors) throws Exception {
+        requestValidator.validateId(VALIDATIONS_POST_ID, input.getId(), errors);
+        DataValidationUtil.validate(errors, VALIDATIONS_POST_ID);
+        return service.performRequestValidations(input.getRequest());
+    }
 
-	@PostMapping(value = "/validateResponse")
-	public ResponseWrapper<ValidationResponseDto> validateResponse(
-			@RequestBody @Valid RequestWrapper<ValidationInputDto> input, Errors errors) throws Exception {
-		requestValidator.validateId(VALIDATIONS_POST_ID, input.getId(), errors);
-		DataValidationUtil.validate(errors, VALIDATIONS_POST_ID);
-		return service.performValidations(input.getRequest());
-	}
+    @PostMapping(value = "/validateResponse")
+    public ResponseWrapper<ValidationResponseDto> validateResponse(
+            @RequestBody @Valid RequestWrapper<ValidationInputDto> input, Errors errors) throws Exception {
+        requestValidator.validateId(VALIDATIONS_POST_ID, input.getId(), errors);
+        DataValidationUtil.validate(errors, VALIDATIONS_POST_ID);
+        return service.performValidations(input.getRequest());
+    }
 
-	@GetMapping(value = "/getSbiTestCases")
-	public ResponseWrapper<List<TestCaseDto>> getSbiTestCases(@RequestParam(required = true) String specVersion,
-			@RequestParam(required = true) String purpose, @RequestParam(required = true) String deviceType,
-			@RequestParam(required = true) String deviceSubType) {
-		try {
-			return service.getSbiTestCases(specVersion, purpose, deviceType, deviceSubType);
-		} catch (Exception ex) {
-			ResponseWrapper<List<TestCaseDto>> responseWrapper = new ResponseWrapper<>();
-			responseWrapper.setId(getTestCasesId);
-			responseWrapper.setResponse(null);
-			List<ServiceError> serviceErrorsList = new ArrayList<>();
-			ServiceError serviceError = new ServiceError();
-			serviceError.setErrorCode(ToolkitErrorCodes.GET_TEST_CASE_ERROR.getErrorCode());
-			serviceError.setMessage(
-					ToolkitErrorCodes.GET_TEST_CASE_ERROR.getErrorMessage() + " " + ex.getLocalizedMessage());
-			serviceErrorsList.add(serviceError);
-			responseWrapper.setErrors(serviceErrorsList);
-			responseWrapper.setVersion(AppConstants.VERSION);
-			responseWrapper.setResponsetime(LocalDateTime.now());
-			return responseWrapper;
-		}
-	}
+    @GetMapping(value = "/getSbiTestCases")
+    public ResponseWrapper<List<TestCaseDto>> getSbiTestCases(@RequestParam(required = true) String specVersion,
+                                                              @RequestParam(required = true) String purpose, @RequestParam(required = true) String deviceType,
+                                                              @RequestParam(required = true) String deviceSubType) {
+        try {
+            return service.getSbiTestCases(specVersion, purpose, deviceType, deviceSubType);
+        } catch (Exception ex) {
+            ResponseWrapper<List<TestCaseDto>> responseWrapper = new ResponseWrapper<>();
+            responseWrapper.setId(getTestCasesId);
+            responseWrapper.setResponse(null);
+            List<ServiceError> serviceErrorsList = new ArrayList<>();
+            ServiceError serviceError = new ServiceError();
+            serviceError.setErrorCode(ToolkitErrorCodes.GET_TEST_CASE_ERROR.getErrorCode());
+            serviceError.setMessage(
+                    ToolkitErrorCodes.GET_TEST_CASE_ERROR.getErrorMessage() + " " + ex.getLocalizedMessage());
+            serviceErrorsList.add(serviceError);
+            responseWrapper.setErrors(serviceErrorsList);
+            responseWrapper.setVersion(AppConstants.VERSION);
+            responseWrapper.setResponsetime(LocalDateTime.now());
+            return responseWrapper;
+        }
+    }
 
-	@GetMapping(value = "/getSdkTestCases")
-	public ResponseWrapper<List<TestCaseDto>> getSdkTestCases(@RequestParam(required = true) String specVersion,
-															  @RequestParam(required = true) String sdkPurpose) {
-		try {
-			return service.getSdkTestCases(specVersion, sdkPurpose);
-		} catch (Exception ex) {
-			ResponseWrapper<List<TestCaseDto>> responseWrapper = new ResponseWrapper<>();
-			responseWrapper.setId(getTestCasesId);
-			responseWrapper.setResponse(null);
-			List<ServiceError> serviceErrorsList = new ArrayList<>();
-			ServiceError serviceError = new ServiceError();
-			serviceError.setErrorCode(ToolkitErrorCodes.GET_TEST_CASE_ERROR.getErrorCode());
-			serviceError.setMessage(
-					ToolkitErrorCodes.GET_TEST_CASE_ERROR.getErrorMessage() + " " + ex.getLocalizedMessage());
-			serviceErrorsList.add(serviceError);
-			responseWrapper.setErrors(serviceErrorsList);
-			responseWrapper.setVersion(AppConstants.VERSION);
-			responseWrapper.setResponsetime(LocalDateTime.now());
-			return responseWrapper;
-		}
-	}
+    @GetMapping(value = "/getSdkTestCases")
+    public ResponseWrapper<List<TestCaseDto>> getSdkTestCases(@RequestParam(required = true) String specVersion,
+                                                              @RequestParam(required = true) String sdkPurpose) {
+        try {
+            return service.getSdkTestCases(specVersion, sdkPurpose);
+        } catch (Exception ex) {
+            ResponseWrapper<List<TestCaseDto>> responseWrapper = new ResponseWrapper<>();
+            responseWrapper.setId(getTestCasesId);
+            responseWrapper.setResponse(null);
+            List<ServiceError> serviceErrorsList = new ArrayList<>();
+            ServiceError serviceError = new ServiceError();
+            serviceError.setErrorCode(ToolkitErrorCodes.GET_TEST_CASE_ERROR.getErrorCode());
+            serviceError.setMessage(
+                    ToolkitErrorCodes.GET_TEST_CASE_ERROR.getErrorMessage() + " " + ex.getLocalizedMessage());
+            serviceErrorsList.add(serviceError);
+            responseWrapper.setErrors(serviceErrorsList);
+            responseWrapper.setVersion(AppConstants.VERSION);
+            responseWrapper.setResponsetime(LocalDateTime.now());
+            return responseWrapper;
+        }
+    }
 
-	@GetMapping(value = "/generateRequestForSDK")
-	public ResponseWrapper<String> generateRequestForSDK(@RequestParam(required = true) String methodName,
-			@RequestParam(required = true) String testcaseId, @RequestParam(required = true) List<String> modalities)
-			throws Exception {
-		return service.generateRequestForSDKTestcase(methodName, testcaseId, modalities);
-	}
+    @GetMapping(value = "/generateRequestForSDK")
+    public ResponseWrapper<String> generateRequestForSDK(@RequestParam(required = true) String methodName,
+                                                         @RequestParam(required = true) String testcaseId, @RequestParam(required = true) List<String> modalities)
+            throws Exception {
+        return service.generateRequestForSDKTestcase(methodName, testcaseId, modalities);
+    }
 
-	@PostMapping(value = "/saveTestCases", produces = "application/json")
-	public ResponseWrapper<TestCaseResponseDto> saveTestCases(
-			@RequestBody @Valid RequestWrapper<TestCaseRequestDto> testCaseRequestDto) throws Exception {
-		return service.saveTestCases(testCaseRequestDto.getRequest().getTestCases());
-	}
+    @PostMapping(value = "/saveTestCases", produces = "application/json")
+    public ResponseWrapper<TestCaseResponseDto> saveTestCases(
+            @RequestBody @Valid RequestWrapper<TestCaseRequestDto> testCaseRequestDto) throws Exception {
+        return service.saveTestCases(testCaseRequestDto.getRequest().getTestCases());
+    }
 
-	@GetMapping(value = "/getTestCase/{testId}")
-	public ResponseWrapper<TestCaseDto> getTestCaseById(@PathVariable String testId) {
-		return service.getTestCaseById(testId);
-	}
+    @GetMapping(value = "/getTestCase/{testId}")
+    public ResponseWrapper<TestCaseDto> getTestCaseById(@PathVariable String testId) {
+        return service.getTestCaseById(testId);
+    }
 }
