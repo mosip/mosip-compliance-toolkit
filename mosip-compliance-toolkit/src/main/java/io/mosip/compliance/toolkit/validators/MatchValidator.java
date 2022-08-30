61c38da5de32b94304ab034355cafb0147678885
Mayura Deshmukh <mayura.deshmukh@gmail.com>
2022-08-30 10:28:12 +0530

    MOSIP-23620 added SDK2009

--- mosip-compliance-toolkit/src/main/java/io/mosip/compliance/toolkit/validators/MatchValidator.java
+++ mosip-compliance-toolkit/src/main/java/io/mosip/compliance/toolkit/validators/MatchValidator.java
@@ -1,6 +1,7 @@
 package io.mosip.compliance.toolkit.validators;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -39,40 +40,84 @@ public class MatchValidator extends SDKValidator {
 			int statusCode = Integer.parseInt(mainResponse.get("statusCode").asText());
 
 			if (statusCode >= 200 && statusCode <= 299) {
-				List<Map<BiometricType, Decision>> matchDecisions = Arrays.asList(matchDecisionsArr).stream()
-						.map(matchDecision -> matchDecision.getDecisions()).collect(Collectors.toList());
+				Map<Integer, Map<BiometricType, Boolean>> resultsMap = new HashMap<Integer, Map<BiometricType, Boolean>>();
 
-				for (Map<BiometricType, Decision> matchDecision : matchDecisions) {
-					for (Entry<BiometricType, Decision> entry : matchDecision.entrySet()) {
-						BiometricType biometricType = entry.getKey();
-						String biometricTypeStr = biometricType.value().toLowerCase();
-						String matchValue = entry.getValue().getMatch().name();
-						if (!inputDto.isNegativeTestCase()) {
-							if (Match.MATCHED.toString().equals(matchValue)) {
-								validationResultDto.setStatus(AppConstants.SUCCESS);
-								validationResultDto
-										.setDescription("Positive Match for " + biometricTypeStr + " is successful");
+				Map<Integer, List<MatchDecision>> galleryDecisionsMap = new HashMap<Integer, List<MatchDecision>>();
+				for (MatchDecision item : matchDecisionsArr) {
+					Integer galleryIndex = Integer.valueOf(item.getGalleryIndex());
+					if (galleryDecisionsMap.get(galleryIndex) != null) {
+						List<MatchDecision> list = galleryDecisionsMap.get(galleryIndex);
+						list.add(item);
+						galleryDecisionsMap.put(galleryIndex, list);
+					} else {
+						List<MatchDecision> newList = new ArrayList<MatchDecision>();
+						newList.add(item);
+						galleryDecisionsMap.put(galleryIndex, newList);
+					}
+				}
+				for (Map.Entry<Integer, List<MatchDecision>> entry : galleryDecisionsMap.entrySet()) {
+					Integer galleryIndex = entry.getKey();
+					List<Map<BiometricType, Decision>> matchDecisions = entry.getValue().stream()
+							.map(matchDecision -> matchDecision.getDecisions()).collect(Collectors.toList());
+					for (Map<BiometricType, Decision> matchDecision : matchDecisions) {
+						for (Entry<BiometricType, Decision> entry1 : matchDecision.entrySet()) {
+							BiometricType biometricType = entry1.getKey();
+							String matchValue = entry1.getValue().getMatch().name();
+							if (!inputDto.isNegativeTestCase()) {
+								if (Match.MATCHED.toString().equals(matchValue)) {
+									setResults(resultsMap, galleryIndex, biometricType, Boolean.TRUE);
+								} else {
+									setResults(resultsMap, galleryIndex, biometricType, Boolean.FALSE);
+								}
 							} else {
-								validationResultDto.setStatus(AppConstants.FAILURE);
-								validationResultDto.setDescription("Positive Match for " + biometricTypeStr
-										+ " failed. Received match decision: " + matchValue);
-							}
-						} else {
-							if (Match.NOT_MATCHED.toString().equals(matchValue)) {
-								validationResultDto.setStatus(AppConstants.SUCCESS);
-								validationResultDto
-										.setDescription("Negative Match for " + biometricTypeStr + " is successful");
-							} else {
-								validationResultDto.setStatus(AppConstants.FAILURE);
-								validationResultDto.setDescription("Negative Match for " + biometricTypeStr
-										+ " failed. Received match decision: " + matchValue);
+								if (Match.NOT_MATCHED.toString().equals(matchValue)) {
+									setResults(resultsMap, galleryIndex, biometricType, Boolean.TRUE);
+								} else {
+									setResults(resultsMap, galleryIndex, biometricType, Boolean.FALSE);
+								}
 							}
 						}
 					}
 				}
+
+				boolean positiveSuccess = false;
+				boolean negativeSuccess = false;
+				String results = "";
+				for (Map.Entry<Integer, Map<BiometricType, Boolean>> rsEntry : resultsMap.entrySet()) {
+					Integer galleryIndex = rsEntry.getKey();
+					Map<BiometricType, Boolean> bioMap = rsEntry.getValue();
+					for (Map.Entry<BiometricType, Boolean> entry2 : bioMap.entrySet()) {
+						if (entry2.getValue()) {
+							if (!inputDto.isNegativeTestCase()) {
+								positiveSuccess = true;
+								results += "Positive Match for " + entry2.getKey().toString()
+										+ " is successful for galleryIndex: " + galleryIndex;
+							} else {
+								negativeSuccess = true;
+								results += "Negative Match for " + entry2.getKey().toString()
+										+ " is successful for galleryIndex: " + galleryIndex;
+							}
+						} else {
+							if (!inputDto.isNegativeTestCase()) {
+								results += "Positive Match for " + entry2.getKey().toString()
+										+ " failed for galleryIndex: " + galleryIndex;
+							} else {
+								results += "Negative Match for " + entry2.getKey().toString()
+										+ " failed for galleryIndex: " + galleryIndex;
+							}
+						}
+						results += "\r\n";
+					}
+				}
+				if (positiveSuccess || negativeSuccess) {
+					validationResultDto.setStatus(AppConstants.SUCCESS);
+				} else {
+					validationResultDto.setStatus(AppConstants.FAILURE);
+				}
+				validationResultDto.setDescription(results);
 			} else {
 				validationResultDto.setStatus(AppConstants.FAILURE);
-				validationResultDto.setDescription("Quality Check status code failed, received: " + statusCode);
+				validationResultDto.setDescription("Match status code failed, received: " + statusCode);
 			}
 		} catch (
 
@@ -82,6 +127,19 @@ public class MatchValidator extends SDKValidator {
 			return validationResultDto;
 		}
 		return validationResultDto;
+	}
+
+	private void setResults(Map<Integer, Map<BiometricType, Boolean>> resultsMap, Integer galleryIndex,
+			BiometricType biometricType, Boolean result) {
+		if (resultsMap.get(galleryIndex) != null) {
+			Map<BiometricType, Boolean> bioResults = resultsMap.get(galleryIndex);
+			bioResults.put(biometricType, result);
+			resultsMap.put(galleryIndex, bioResults);
+		} else {
+			Map<BiometricType, Boolean> bioResults = new HashMap<BiometricType, Boolean>();
+			bioResults.put(biometricType, result);
+			resultsMap.put(galleryIndex, bioResults);
+		}
 	}
 
 }
