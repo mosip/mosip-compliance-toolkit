a1c2bbe39156afd8c7cb01223937caafa745cff5
JanardhanBS-SyncByte <janardhan@syncbyte.in>
2022-09-27 19:23:21 +0530

    testcases and testdata

---
+++ mosip-compliance-toolkit/src/main/java/io/mosip/compliance/toolkit/validators/ExtractTemplateValidator.java
@@ -0,0 +1,38 @@
+package io.mosip.compliance.toolkit.validators;
+
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import io.mosip.compliance.toolkit.constants.AppConstants;
+import io.mosip.compliance.toolkit.dto.testcases.ValidationInputDto;
+import io.mosip.compliance.toolkit.dto.testcases.ValidationResultDto;
+
+@Component
+public class ExtractTemplateValidator extends ToolkitValidator {
+
+	public ValidationResultDto validateResponse(ValidationInputDto inputDto) {
+		ValidationResultDto validationResultDto = new ValidationResultDto();
+		try {
+			ObjectNode methodResponse = (ObjectNode) objectMapperConfig.objectMapper()
+					.readValue(inputDto.getMethodResponse(), ObjectNode.class);
+
+			JsonNode mainResponse = (JsonNode) methodResponse.get("response");
+
+			int statusCode = Integer.parseInt(mainResponse.get("statusCode").asText());
+			if (statusCode >= 200 && statusCode <= 299) {
+				validationResultDto.setStatus(AppConstants.SUCCESS);
+				validationResultDto.setDescription("Segment validation is successful");
+			} else {
+				validationResultDto.setStatus(AppConstants.FAILURE);
+				validationResultDto.setDescription("Segment status code failed, received: " + statusCode);
+			}
+		} catch (Exception e) {
+			validationResultDto.setStatus(AppConstants.FAILURE);
+			validationResultDto.setDescription(e.getLocalizedMessage());
+			return validationResultDto;
+		}
+		return validationResultDto;
+	}
+}
