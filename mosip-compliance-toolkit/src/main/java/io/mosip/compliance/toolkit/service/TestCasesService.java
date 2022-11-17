b700819a740251dc7dec1bcce5e493a2f071c75b
srinivas-mosip <srinivas.mosip@gmail.com>
2022-11-17 12:16:31 +0530

    MOSIP-24725 : security hotspot fix

--- mosip-compliance-toolkit/src/main/java/io/mosip/compliance/toolkit/service/TestCasesService.java
+++ mosip-compliance-toolkit/src/main/java/io/mosip/compliance/toolkit/service/TestCasesService.java
@@ -71,7 +71,6 @@ import io.mosip.compliance.toolkit.exceptions.ToolkitException;
 import io.mosip.compliance.toolkit.repository.BiometricTestDataRepository;
 import io.mosip.compliance.toolkit.repository.TestCasesRepository;
 import io.mosip.compliance.toolkit.util.CryptoUtil;
-import io.mosip.compliance.toolkit.util.ZipFileUtil;
 import io.mosip.compliance.toolkit.validators.BaseValidator;
 import io.mosip.kernel.biometrics.constant.BiometricType;
 import io.mosip.kernel.biometrics.entities.BiometricRecord;
@@ -917,17 +916,42 @@ public class TestCasesService {
             }
             xmlFileName += name;
             ZipEntry zipEntry = null;
-            // first validate for Zip Bomb Attack
-            ZipFileUtil.checkForZipBombAttack(zis);
-            zis.reset();
+            
+        	double THRESHOLD_RATIO = 10;
+			int THRESHOLD_ENTRIES = 10000;
+			int THRESHOLD_SIZE = 1000000000; // 1 GB
+			
+			int totalSizeArchive = 0;
+			int totalEntryArchive = 0;
+            
             while ((zipEntry = zis.getNextEntry()) != null) {
+            	totalEntryArchive++;
+				if (totalEntryArchive > THRESHOLD_ENTRIES) {
+					throw new ToolkitException(ToolkitErrorCodes.ZIP_ENTRIES_TOO_MANY_ERROR.getErrorCode(),
+							ToolkitErrorCodes.ZIP_ENTRIES_TOO_MANY_ERROR.getErrorMessage());
+				}
                 if (xmlFileName == null || (xmlFileName != null && xmlFileName.equals(zipEntry.getName()))) {
-                    int nBytes = -1;
-                    byte[] buffer = new byte[2048];
-                    while ((nBytes = zis.read(buffer)) > 0) { // Compliant
-                        out.write(buffer, 0, nBytes);
-                    }
-                    bytes = out.toByteArray();
+                	int nBytes = -1;
+					byte[] buffer = new byte[2048];
+					double totalSizeEntry = 0;
+					while ((nBytes = zis.read(buffer)) > 0) {
+						out.write(buffer, 0, nBytes);
+						totalSizeEntry += nBytes;
+						totalSizeArchive += nBytes;
+						double compressionRatio = totalSizeEntry / zipEntry.getCompressedSize();
+						if (compressionRatio > THRESHOLD_RATIO) {
+							// ratio between compressed and uncompressed data is highly suspicious, looks
+							// like a Zip Bomb Attack
+							throw new ToolkitException(ToolkitErrorCodes.ZIP_HIGH_COMPRESSION_RATIO_ERROR.getErrorCode(),
+									ToolkitErrorCodes.ZIP_HIGH_COMPRESSION_RATIO_ERROR.getErrorMessage());
+						}
+					}
+					bytes = out.toByteArray();
+
+					if (totalSizeArchive > THRESHOLD_SIZE) {
+						throw new ToolkitException(ToolkitErrorCodes.ZIP_SIZE_TOO_LARGE_ERROR.getErrorCode(),
+								ToolkitErrorCodes.ZIP_SIZE_TOO_LARGE_ERROR.getErrorMessage());
+					}
                     break;
                 }
             }
