ec0af0e28c00f41b50f8a778a51e2c1ca2e740ce
JanardhanBS-SyncByte <janardhan@syncbyte.in>
2022-12-15 12:49:00 +0530

    MOSIP-25233

--- mosip-compliance-toolkit/src/main/java/io/mosip/compliance/toolkit/util/CryptoUtil.java
+++ mosip-compliance-toolkit/src/main/java/io/mosip/compliance/toolkit/util/CryptoUtil.java
@@ -1,5 +1,8 @@
 package io.mosip.compliance.toolkit.util;
 
+import static io.restassured.RestAssured.given;
+
+import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -12,7 +15,19 @@ import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Hex;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import io.mosip.compliance.toolkit.validators.SBIValidator.DecryptValidatorDto;
+import io.mosip.compliance.toolkit.validators.SBIValidator.DeviceValidatorDto;
+import io.mosip.kernel.core.util.DateUtils;
+import io.restassured.http.Cookie;
+
 public final class CryptoUtil {
+	private static String AUTH_REQ_TEMPLATE = "{ \"id\": \"string\",\"metadata\": {},\"request\": { \"appId\": \"%s\", \"clientId\": \"%s\", \"secretKey\": \"%s\" }, \"requesttime\": \"%s\", \"version\": \"string\"}";
 
 	public static String getEncodedHash(byte[] bytes) throws NoSuchAlgorithmException, NoSuchProviderException {
 		Security.addProvider(new BouncyCastleProvider());
@@ -23,6 +38,41 @@ public final class CryptoUtil {
 		digest.update(bytes);
 		byte[] hash = digest.digest();
 		return Base64.getUrlEncoder().encodeToString(hash);
+	}
+
+	public static io.restassured.response.Response getDecryptPostResponse(String authUrl, String appId, String clientId,
+			String secretKey, String postUrl, DecryptValidatorDto decryptValidatorDto) throws IOException {
+		Cookie.Builder builder = new Cookie.Builder("Authorization", getAuthToken(authUrl, appId, clientId, secretKey));
+
+		return given().cookie(builder.build()).relaxedHTTPSValidation().body(decryptValidatorDto)
+				.contentType("application/json").log().all().when().post(postUrl).then().log().all().extract()
+				.response();
+	}
+
+	public static io.restassured.response.Response getTrustRootPostResponse(String authUrl, String appId, String clientId,
+			String secretKey, String postUrl, DeviceValidatorDto deviceValidatorDto) throws IOException {
+		Cookie.Builder builder = new Cookie.Builder("Authorization", getAuthToken(authUrl, appId, clientId, secretKey));
+
+		return given().cookie(builder.build()).relaxedHTTPSValidation().body(deviceValidatorDto)
+				.contentType("application/json").log().all().when().post(postUrl).then().log().all().extract()
+				.response();
+	}
+
+	public static String getAuthToken(String authUrl, String appId, String clientId, String secretKey)
+			throws IOException {
+		OkHttpClient client = new OkHttpClient();
+		String requestBody = String.format(AUTH_REQ_TEMPLATE, appId, clientId, secretKey,
+				DateUtils.getUTCCurrentDateTime());
+
+		MediaType mediaType = MediaType.parse("application/json; charset=utf-8");
+		RequestBody body = RequestBody.create(mediaType, requestBody);
+		Request request = new Request.Builder().url(authUrl).post(body).build();
+
+		Response response = client.newCall(request).execute();
+		if (response.isSuccessful()) {
+			return response.header("authorization");
+		}
+		return "";
 	}
 
 	// Function to return the XOR
