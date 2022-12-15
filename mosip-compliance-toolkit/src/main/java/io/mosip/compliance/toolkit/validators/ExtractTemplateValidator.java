b2e59af67f55f29f682957eacab659de0145e143
JanardhanBS-SyncByte <janardhan@syncbyte.in>
2022-12-15 12:56:07 +0530

    MOSIP-25233

--- mosip-compliance-toolkit/src/main/java/io/mosip/compliance/toolkit/validators/ExtractTemplateValidator.java
+++ mosip-compliance-toolkit/src/main/java/io/mosip/compliance/toolkit/validators/ExtractTemplateValidator.java
@@ -23,10 +23,10 @@ public class ExtractTemplateValidator extends ToolkitValidator {
 			int statusCode = Integer.parseInt(mainResponse.get("statusCode").asText());
 			if (statusCode >= 200 && statusCode <= 299) {
 				validationResultDto.setStatus(AppConstants.SUCCESS);
-				validationResultDto.setDescription("Segment validation is successful");
+				validationResultDto.setDescription("Extract Template validation is successful");
 			} else {
 				validationResultDto.setStatus(AppConstants.FAILURE);
-				validationResultDto.setDescription("Segment status code failed, received: " + statusCode);
+				validationResultDto.setDescription("Extract Template status code failed, received: " + statusCode);
 			}
 		} catch (Exception e) {
 			validationResultDto.setStatus(AppConstants.FAILURE);
