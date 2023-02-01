6adccd06a586479ace2c6e5fff0d78490d69c0f0
SwethaKrish4 <110164892+SwethaKrish4@users.noreply.github.com>
2023-02-01 12:49:47 +0530

    MOSIP-25267 : Added TimeCheckValidator

---
+++ mosip-compliance-toolkit/src/main/java/io/mosip/compliance/toolkit/validators/TimeCheckValidator.java
@@ -0,0 +1,82 @@
+package io.mosip.compliance.toolkit.validators;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import io.mosip.compliance.toolkit.constants.AppConstants;
+import io.mosip.compliance.toolkit.dto.testcases.ValidationInputDto;
+import io.mosip.compliance.toolkit.dto.testcases.ValidationResultDto;
+import io.mosip.compliance.toolkit.exceptions.ToolkitException;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class TimeCheckValidator extends SBIValidator{
+    @Value("${mosip.toolkit.sbi.timestamp-interval}")
+    String interval;
+
+    @Override
+    public ValidationResultDto validateResponse(ValidationInputDto inputDto) {
+        ValidationResultDto validationResultDto = new ValidationResultDto();
+        try {
+            ObjectNode extraInfo = (ObjectNode) objectMapperConfig.objectMapper()
+                    .readValue(inputDto.getExtraInfoJson(), ObjectNode.class);
+            String startTime=extraInfo.get("startExecutionTime").asText();
+            LocalDateTime localStartTime=getLocalDate(startTime);
+
+            long time_interval=Long.parseLong(interval);
+
+            String responseJson = inputDto.getMethodResponse();
+            ObjectNode captureInfoResponse = (ObjectNode) objectMapperConfig.objectMapper()
+                    .readValue(responseJson, ObjectNode.class);
+            JsonNode arrBiometricNodes = captureInfoResponse.get(BIOMETRICS);
+            if (!arrBiometricNodes.isNull() && arrBiometricNodes.isArray()) {
+                boolean result=false;
+                long diff=0;
+                for (final JsonNode biometricNode : arrBiometricNodes) {
+                    JsonNode dataNode = biometricNode.get(DECODED_DATA);
+
+                    String timestamp = dataNode.get(TIME_STAMP).asText();
+                    LocalDateTime localtimestamp = getLocalDate1(timestamp);
+                    diff = Duration.between(localStartTime, localtimestamp).toMinutes();
+                    if (diff <=time_interval) {
+                        result = true;
+                    } else {
+                        result = false;
+                        break;
+                    }
+                }
+                if(result==false){
+                    validationResultDto.setStatus(AppConstants.FAILURE);
+                    validationResultDto.setDescription("Time Check validation failed. Timestamp interval given: " + interval + "m." + " Response received in: " + diff + "m");
+                }else{
+                    validationResultDto.setStatus(AppConstants.SUCCESS);
+                    validationResultDto.setDescription("Time Check validation is successful");
+                }
+
+            }
+        }catch (ToolkitException e) {
+            validationResultDto.setStatus(AppConstants.FAILURE);
+            validationResultDto.setDescription(e.getLocalizedMessage());
+        } catch (Exception e) {
+            validationResultDto.setStatus(AppConstants.FAILURE);
+            validationResultDto.setDescription(e.getLocalizedMessage());
+        }
+        return validationResultDto;
+    }
+
+    private LocalDateTime getLocalDate(String time) {
+        String DATEFORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern(DATEFORMAT);
+        LocalDateTime localDt = LocalDateTime.parse(time, dateFormat);
+        return localDt;
+    }
+    private LocalDateTime getLocalDate1(String time) {
+        String DATEFORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'";
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern(DATEFORMAT);
+        LocalDateTime localDt = LocalDateTime.parse(time, dateFormat);
+        return localDt;
+    }
+
+}
