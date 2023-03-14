bdbeb263ce6f1cffb74f3d01885b9bfea043ddea
Rakshitha650 <rakshitham38@gmail.com>
2023-03-14 15:24:26 +0530

    [MOSIP-26493] Added toolkit-android-client

--- helm/keycloak-init.sh
+++ helm/keycloak-init.sh
@@ -10,7 +10,8 @@ echo checking if toolkit client is created already
 IAMHOST_URL=$(kubectl get cm global -o jsonpath={.data.mosip-iam-external-host})
 TOOLKIT_CLIENT_SECRET_KEY="mosip_toolkit_client_secret"
 TOOLKIT_CLIENT_SECRET_VALUE=$( kubectl -n keycloak get secret keycloak-client-secrets -o jsonpath={.data.mosip_toolkit_client_secret} | base64 -d )
-
+TOOLKIT_ANDROID_CLIENT_SECRET_KEY="mosip_toolkit_android_client_secret"
+TOOLKIT_ANDROID_CLIENT_SECRET_VALUE=$( kubectl -n keycloak get secret keycloak-client-secrets -o jsonpath={.data.mosip_toolkit_android_client_secret} | base64 -d )
 echo Creating Toolkit keycloak client
 echo "Copy keycloak configmap and secrets"
 $COPY_UTIL configmap keycloak-host keycloak $NS
@@ -25,12 +26,17 @@ helm -n $NS install toolkit-keycloak-init mosip/keycloak-init \
 --set frontend="https://$IAMHOST_URL/auth" \
 --set clientSecrets[0].name="$TOOLKIT_CLIENT_SECRET_KEY" \
 --set clientSecrets[0].secret="$TOOLKIT_CLIENT_SECRET_VALUE" \
+--set clientSecrets[1].name="$TOOLKIT_ANDROID_CLIENT_SECRET_KEY" \
+--set clientSecrets[1].secret="$TOOLKIT_ANDROID_CLIENT_SECRET_VALUE" \
 --version $CHART_VERSION --wait
 
 TOOLKIT_CLIENT_SECRET_VALUE=$( kubectl -n $NS get secret keycloak-client-secrets -o json |  jq ".data.$TOOLKIT_CLIENT_SECRET_KEY" )
+TOOLKIT_ANDROID_CLIENT_SECRET_VALUE=$( kubectl -n $NS get secret keycloak-client-secrets -o json | jq ".data.$TOOLKIT_ANDROID_CLIENT_SECRET_KEY" )
 
 kubectl -n keycloak get secret keycloak-client-secrets -o json | jq ".data[\"$TOOLKIT_CLIENT_SECRET_KEY\"]=$TOOLKIT_CLIENT_SECRET_VALUE" | kubectl apply -f -
 kubectl -n config-server get secret keycloak-client-secrets -o json | jq ".data[\"$TOOLKIT_CLIENT_SECRET_KEY\"]=$TOOLKIT_CLIENT_SECRET_VALUE" | kubectl apply -f -
+kubectl -n keycloak get secret keycloak-client-secrets -o json | jq ".data[\"$TOOLKIT_ANDROID_CLIENT_SECRET_KEY\"]=$TOOLKIT_ANDROID_CLIENT_SECRET_VALUE" | kubectl apply -f -
+kubectl -n config-server get secret keycloak-client-secrets -o json | jq ".data[\"$TOOLKIT_ANDROID_CLIENT_SECRET_KEY\"]=$TOOLKIT_ANDROID_CLIENT_SECRET_VALUE" | kubectl apply -f -
 
 echo "Check the existence of the toolkit secret & host placeholder & pass the toolkit secret & toolkit host to config-server deployment if the placeholder does not exist."
 TOOLKIT_HOST=$( kubectl -n config-server get deployment -o json | jq -c '.items[].spec.template.spec.containers[].env[]| select(.name == "SPRING_CLOUD_CONFIG_SERVER_OVERRIDES_MOSIP_COMPLIANCE_HOST")|.name' )
@@ -46,3 +52,9 @@ if [[ -z $TOOLKIT_SECRET ]]; then
   kubectl -n config-server rollout status deploy/config-server
 fi
 
+TOOLKIT_ANDROID_SECRET=$( kubectl -n config-server get deployment -o json | jq -c '.items[].spec.template.spec.containers[].env[]| select(.name == "SPRING_CLOUD_CONFIG_SERVER_OVERRIDES_MOSIP_ANDROID_TOOLKIT_CLIENT_SECRET")|.name' )
+if [[ -z $TOOLKIT_ANDROID_SECRET ]]; then
+  kubectl -n config-server set env --keys=mosip_toolkit_android_client_secret --from secret/keycloak-client-secrets deployment/config-server --prefix=SPRING_CLOUD_CONFIG_SERVER_OVERRIDES_
+  echo "Waiting for config-server to be Up and running"
+  kubectl -n config-server rollout status deploy/config-server
+fi
