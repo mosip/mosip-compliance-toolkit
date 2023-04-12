8d3a571500993f6d1a3edd6b564c6739f6f9c1d8
Nikhil <nikhilreddy64@gmail.com>
2023-04-12 11:32:05 +0530

    added description key for facilitating translation in UI

--- mosip-compliance-toolkit/src/main/java/io/mosip/compliance/toolkit/constants/AppConstants.java
+++ mosip-compliance-toolkit/src/main/java/io/mosip/compliance/toolkit/constants/AppConstants.java
@@ -35,4 +35,6 @@ public class AppConstants {
 	public static final String RS256_ALGORITHM_TYPE = "RS256";
 
 	public static final String VERSION = "1.0";
+
+	public static final String DESCRIPTION_CODE= "SCHEMA_VALIDATOR_001";
 }
