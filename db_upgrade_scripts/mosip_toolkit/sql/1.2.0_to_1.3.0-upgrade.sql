c4484868a5e0b9d78dc57f8e45a50a66cbe71ace
SwethaKrish4 <110164892+SwethaKrish4@users.noreply.github.com>
2023-06-21 16:53:28 +0530

    added default value

--- db_upgrade_scripts/mosip_toolkit/sql/1.2.0_to_1.3.0-upgrade.sql
+++ db_upgrade_scripts/mosip_toolkit/sql/1.2.0_to_1.3.0-upgrade.sql
@@ -1,4 +1,4 @@
 \c mosip_toolkit sysadmin
 
-ALTER TABLE toolkit.abis_projects Add COLUMN modality character varying(256) NOT NULL;
+ALTER TABLE toolkit.abis_projects Add COLUMN modality character varying(256) NOT NULL DEFAULT 'All';
 COMMENT ON COLUMN toolkit.abis_projects.modality IS 'modality: different modalities combination';
