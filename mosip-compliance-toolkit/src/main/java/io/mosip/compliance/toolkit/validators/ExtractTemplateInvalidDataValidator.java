b8e82cf760e2164e885c9c64c59b348c4fd0aa2b
SwethaKrish4 <110164892+SwethaKrish4@users.noreply.github.com>
2023-06-28 12:20:50 +0530

    duplication fix

--- mosip-compliance-toolkit/src/main/java/io/mosip/compliance/toolkit/validators/ExtractTemplateInvalidDataValidator.java
+++ mosip-compliance-toolkit/src/main/java/io/mosip/compliance/toolkit/validators/ExtractTemplateInvalidDataValidator.java
@@ -1,35 +1,9 @@
 package io.mosip.compliance.toolkit.validators;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-
-import io.mosip.compliance.toolkit.constants.AppConstants;
-import io.mosip.compliance.toolkit.dto.testcases.ValidationInputDto;
-import io.mosip.compliance.toolkit.dto.testcases.ValidationResultDto;
-
-public class ExtractTemplateInvalidDataValidator extends SDKValidator {
+public class ExtractTemplateInvalidDataValidator extends InvalidDataValidator {
 
 	@Override
-	public ValidationResultDto validateResponse(ValidationInputDto inputDto) {
-		ValidationResultDto validationResultDto = new ValidationResultDto();
-		try {
-			int statusCode = getStatusCode(inputDto);
-			if (statusCode == 401 || statusCode == 403) {
-				validationResultDto.setStatus(AppConstants.SUCCESS);
-				validationResultDto.setDescription(successInvalidDataDescription + statusCode);
-				validationResultDto.setDescriptionKey(successInvalidDataDescriptionKey + statusCode);
-			} else {
-				validationResultDto.setStatus(AppConstants.FAILURE);
-				validationResultDto.setDescription(failureInvalidDataDescription + statusCode);
-				validationResultDto.setDescriptionKey(failureInvalidDataDescriptionKey + statusCode);
-			}
-		} catch (Exception e) {
-			validationResultDto.setStatus(AppConstants.FAILURE);
-			validationResultDto.setDescription(e.getLocalizedMessage());
-			validationResultDto.setDescriptionKey(e.getLocalizedMessage());
-			return validationResultDto;
-		}
-		return validationResultDto;
+	protected boolean isSuccessStatusCode(int statusCode) {
+		return statusCode == 401 || statusCode == 403;
 	}
-
 }
