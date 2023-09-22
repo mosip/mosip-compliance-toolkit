baa6f09ccc74fa635157752f8dc83f373375fa65
Rakshithb1 <79500257+Rakshithb1@users.noreply.github.com>
2023-09-22 20:02:13 +0530

    [MOSIP-27986] Updated init_db.sh (#738)
    Signed-off-by: Rakshithb1 <79500257+Rakshithb1@users.noreply.github.com>

--- db_scripts/init_db.sh
+++ db_scripts/init_db.sh
@@ -15,6 +15,8 @@ while true; do
     read -p "CAUTION: Do we already have Postgres installed? Also make sure the toolkit DB is backed up as the same will be overiden. Do you still want to continue?" yn
     if [ $yn = "Y" ]
       then
+        echo Create $NS namespace
+        kubectl create ns $NS
         echo Removing existing mosip_toolkit DB installation
         helm -n $NS delete postgres-init-toolkit
         echo Copy Postgres secrets
