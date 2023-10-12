81bca9fbf70024ce1e1d51c62740df5f32126339
SwethaKrish4 <110164892+SwethaKrish4@users.noreply.github.com>
2023-10-12 12:46:14 +0530

    MOSIP-29797: ordered upgrade and rollback files
    Signed-off-by: SwethaKrish4 <110164892+SwethaKrish4@users.noreply.github.com>

--- db_upgrade_scripts/mosip_toolkit/sql/1.2.0_to_1.3.0_upgrade.sql
+++ db_upgrade_scripts/mosip_toolkit/sql/1.2.0_to_1.3.0_upgrade.sql
@@ -1,62 +1,14 @@
 \c mosip_toolkit sysadmin
 
+-- add new columns in abis_projects table.
 ALTER TABLE toolkit.abis_projects Add COLUMN modality character varying(256) NOT NULL DEFAULT 'All';
-COMMENT ON COLUMN toolkit.abis_projects.modality IS 'modality: different modalities combination';
-
--- This table has all the data share tokens for a test run for a given collection in ABIS project.
-CREATE TABLE toolkit.datashare_tokens(
-    partner_id character varying(36) NOT NULL,
-    testcase_id character varying(36) NOT NULL,
-    testrun_id character varying(36) NOT NULL,
-    token character varying NOT NULL,
-    result character varying(256),
-    CONSTRAINT datashare_tokens_pk PRIMARY KEY (partner_id, testCase_id, testRun_id) 
-);
-COMMENT ON TABLE toolkit.datashare_tokens IS 'This table has all the data share tokens for a test run for a given collection in ABIS project.';
-COMMENT ON COLUMN toolkit.datashare_tokens.partner_id IS 'Partner Id: partner id who has created this project.';
-COMMENT ON COLUMN toolkit.datashare_tokens.testcase_id IS 'Testcase ID: Id of the corresponding testcase.';
-COMMENT ON COLUMN toolkit.datashare_tokens.testrun_id IS 'testRun_id: Unique run Id generated for an test run.';
-COMMENT ON COLUMN toolkit.datashare_tokens.token IS 'token: Data share token test run';
-COMMENT ON COLUMN toolkit.datashare_tokens.result IS 'result: result of data share tokens.';
-
-ALTER TABLE toolkit.sbi_projects Add COLUMN device_image1 character varying;
-ALTER TABLE toolkit.sbi_projects Add COLUMN device_image2 character varying;
-ALTER TABLE toolkit.sbi_projects Add COLUMN device_image3 character varying;
-ALTER TABLE toolkit.sbi_projects Add COLUMN device_image4 character varying;
-ALTER TABLE toolkit.sbi_projects Add COLUMN sbi_hash character varying NOT NULL DEFAULT 'To_Be_Added';
-ALTER TABLE toolkit.sbi_projects Add COLUMN website_url character varying(256) NOT NULL DEFAULT 'To_Be_Added';
-ALTER TABLE toolkit.sbi_projects Add COLUMN org_name character varying(64) NOT NULL DEFAULT 'Not_Available';
-COMMENT ON COLUMN toolkit.sbi_projects.device_image1 IS 'device_image1: Base64 value of device image';
-COMMENT ON COLUMN toolkit.sbi_projects.device_image2 IS 'device_image2: Base64 value of device image';
-COMMENT ON COLUMN toolkit.sbi_projects.device_image3 IS 'device_image3: Base64 value of device image';
-COMMENT ON COLUMN toolkit.sbi_projects.device_image4 IS 'device_image4: Base64 value of device image';
-COMMENT ON COLUMN toolkit.sbi_projects.sbi_hash IS 'sbi_hash: Encoded hash of SBI installation file';
-COMMENT ON COLUMN toolkit.sbi_projects.website_url IS 'website_url: Partner website url';
-COMMENT ON COLUMN toolkit.sbi_projects.org_name IS 'orgname: organization name to which partner belongs to.';
-
-ALTER TABLE toolkit.sdk_projects Add COLUMN sdk_hash character varying NOT NULL DEFAULT 'To_Be_Added';
-ALTER TABLE toolkit.sdk_projects Add COLUMN website_url character varying(256) NOT NULL DEFAULT 'To_Be_Added';
-COMMENT ON COLUMN toolkit.sdk_projects.sdk_hash IS 'sdk_hash: Encoded hash of SDK installation file';
-COMMENT ON COLUMN toolkit.sdk_projects.website_url IS 'website_url: Partner website url';
-
 ALTER TABLE toolkit.abis_projects Add COLUMN abis_hash character varying NOT NULL DEFAULT 'To_Be_Added';
 ALTER TABLE toolkit.abis_projects Add COLUMN website_url character varying(256) NOT NULL DEFAULT 'To_Be_Added';
+COMMENT ON COLUMN toolkit.abis_projects.modality IS 'modality: different modalities combination';
 COMMENT ON COLUMN toolkit.abis_projects.abis_hash IS 'abis_hash: Encoded hash of ABIS installation file';
 COMMENT ON COLUMN toolkit.abis_projects.website_url IS 'website_url: Partner website url';
 
-ALTER TABLE toolkit.test_run Add COLUMN execution_status character varying(36) NOT NULL DEFAULT 'incomplete';
-ALTER TABLE toolkit.test_run Add COLUMN run_status character varying(36) DEFAULT 'failure';
-COMMENT ON COLUMN toolkit.test_run.execution_status IS 'Execution Status: test run execution status incomplete or complete.';
-COMMENT ON COLUMN toolkit.test_run.run_status IS 'Test Run Status: test run status as failure/success';
-
-ALTER TABLE toolkit.test_run
-    ADD CONSTRAINT test_run_execution_status_values CHECK (execution_status IN ('incomplete','complete'));
-ALTER TABLE toolkit.test_run
-    ADD CONSTRAINT test_run_run_status_values CHECK (run_status IN ('success','failure'));
-
-ALTER TABLE toolkit.collections Add COLUMN collection_type character varying(256) NOT NULL DEFAULT 'custom_collection';
-COMMENT ON COLUMN toolkit.collections.collection_type IS 'Collection Type: this can be custom_collection or compliance_collection';
-
+-- This table has biometric scores of sbi and sdk from biometric quality check validation.
 CREATE TABLE toolkit.biometric_scores(
 	  id character varying(36) NOT NULL,
     project_id character varying(36) NOT NULL,
@@ -78,7 +30,66 @@ COMMENT ON COLUMN toolkit.biometric_scores.cr_by IS 'Created By : ID or name of 
 COMMENT ON COLUMN toolkit.biometric_scores.cr_dtimes IS 'Created DateTimestamp : Date and Timestamp when the record is created/inserted';
 COMMENT ON COLUMN toolkit.biometric_scores.scores_json IS 'Scores Json: Biometric scores of quality check testcases';
 
--- add new column method-id in test_run_details table
+-- add new columns in collections table.
+ALTER TABLE toolkit.collections Add COLUMN collection_type character varying(256) NOT NULL DEFAULT 'custom_collection';
+COMMENT ON COLUMN toolkit.collections.collection_type IS 'Collection Type: this can be custom_collection or compliance_collection';
+
+-- This table has all the data share tokens for a test run for a given collection in ABIS project.
+CREATE TABLE toolkit.datashare_tokens(
+    partner_id character varying(36) NOT NULL,
+    testcase_id character varying(36) NOT NULL,
+    testrun_id character varying(36) NOT NULL,
+    token character varying NOT NULL,
+    result character varying(256),
+    CONSTRAINT datashare_tokens_pk PRIMARY KEY (partner_id, testCase_id, testRun_id) 
+);
+COMMENT ON TABLE toolkit.datashare_tokens IS 'This table has all the data share tokens for a test run for a given collection in ABIS project.';
+COMMENT ON COLUMN toolkit.datashare_tokens.partner_id IS 'Partner Id: partner id who has created this project.';
+COMMENT ON COLUMN toolkit.datashare_tokens.testcase_id IS 'Testcase ID: Id of the corresponding testcase.';
+COMMENT ON COLUMN toolkit.datashare_tokens.testrun_id IS 'testRun_id: Unique run Id generated for an test run.';
+COMMENT ON COLUMN toolkit.datashare_tokens.token IS 'token: Data share token test run';
+COMMENT ON COLUMN toolkit.datashare_tokens.result IS 'result: result of data share tokens.';
+
+-- add new columns in sbi_projects table.
+ALTER TABLE toolkit.sbi_projects Add COLUMN device_image1 character varying;
+ALTER TABLE toolkit.sbi_projects Add COLUMN device_image2 character varying;
+ALTER TABLE toolkit.sbi_projects Add COLUMN device_image3 character varying;
+ALTER TABLE toolkit.sbi_projects Add COLUMN device_image4 character varying;
+ALTER TABLE toolkit.sbi_projects Add COLUMN sbi_hash character varying NOT NULL DEFAULT 'To_Be_Added';
+ALTER TABLE toolkit.sbi_projects Add COLUMN website_url character varying(256) NOT NULL DEFAULT 'To_Be_Added';
+ALTER TABLE toolkit.sbi_projects Add COLUMN org_name character varying(64) NOT NULL DEFAULT 'Not_Available';
+COMMENT ON COLUMN toolkit.sbi_projects.device_image1 IS 'device_image1: Base64 value of device image';
+COMMENT ON COLUMN toolkit.sbi_projects.device_image2 IS 'device_image2: Base64 value of device image';
+COMMENT ON COLUMN toolkit.sbi_projects.device_image3 IS 'device_image3: Base64 value of device image';
+COMMENT ON COLUMN toolkit.sbi_projects.device_image4 IS 'device_image4: Base64 value of device image';
+COMMENT ON COLUMN toolkit.sbi_projects.sbi_hash IS 'sbi_hash: Encoded hash of SBI installation file';
+COMMENT ON COLUMN toolkit.sbi_projects.website_url IS 'website_url: Partner website url';
+COMMENT ON COLUMN toolkit.sbi_projects.org_name IS 'orgname: organization name to which partner belongs to.';
+
+-- add new columns in sdk_projects table.
+ALTER TABLE toolkit.sdk_projects Add COLUMN sdk_hash character varying NOT NULL DEFAULT 'To_Be_Added';
+ALTER TABLE toolkit.sdk_projects Add COLUMN website_url character varying(256) NOT NULL DEFAULT 'To_Be_Added';
+COMMENT ON COLUMN toolkit.sdk_projects.sdk_hash IS 'sdk_hash: Encoded hash of SDK installation file';
+COMMENT ON COLUMN toolkit.sdk_projects.website_url IS 'website_url: Partner website url';
+
+-- add new columns and constraints in test_run table
+ALTER TABLE toolkit.test_run Add COLUMN execution_status character varying(36) NOT NULL DEFAULT 'incomplete';
+ALTER TABLE toolkit.test_run Add COLUMN run_status character varying(36) DEFAULT 'failure';
+COMMENT ON COLUMN toolkit.test_run.execution_status IS 'Execution Status: test run execution status incomplete or complete.';
+COMMENT ON COLUMN toolkit.test_run.run_status IS 'Test Run Status: test run status as failure/success';
+
+ALTER TABLE toolkit.test_run
+    ADD CONSTRAINT test_run_execution_status_values CHECK (execution_status IN ('incomplete','complete'));
+ALTER TABLE toolkit.test_run
+    ADD CONSTRAINT test_run_run_status_values CHECK (run_status IN ('success','failure'));
+	
+-- add new columns in test_run_archive table
+ALTER TABLE toolkit.test_run_archive ADD COLUMN execution_status character varying(36) NOT NULL DEFAULT 'incomplete';
+ALTER TABLE toolkit.test_run_archive ADD COLUMN result_status character varying(36) NOT NULL DEFAULT 'failure';
+COMMENT ON COLUMN toolkit.test_run_archive.execution_status IS 'Execution Status: test run execution status Incomplete or Complete.';
+COMMENT ON COLUMN toolkit.test_run_archive.run_status IS 'Test Run Status: test run status as Failure/Success';
+
+-- add new columns in test_run_details table
 ALTER TABLE toolkit.test_run_details ADD COLUMN method_id character varying(150) NOT NULL DEFAULT 'Not_Available';
 ALTER TABLE toolkit.test_run_details ADD COLUMN execution_status character varying(36) NOT NULL DEFAULT 'Not_Available';
 COMMENT ON COLUMN toolkit.test_run_details.method_id IS 'Method ID: Unique method Id created for each method response';
@@ -97,7 +108,7 @@ ALTER TABLE toolkit.test_run_details
 ALTER TABLE toolkit.test_run_details
   ADD CONSTRAINT test_run_details_result_status_values CHECK (result_status IN ('success','failure'));
 
--- add new column method-id in test_run_details_archive table
+-- add new columns in test_run_details_archive table
 ALTER TABLE toolkit.test_run_details_archive ADD COLUMN method_id character varying(150) NOT NULL DEFAULT 'Not_Available';
 ALTER TABLE toolkit.test_run_details_archive ADD COLUMN execution_status character varying(36) NOT NULL DEFAULT 'Not_Available';
 COMMENT ON COLUMN toolkit.test_run_details_archive.method_id IS 'Method ID: Unique method Id created for each method response';
@@ -110,12 +121,6 @@ DROP CONSTRAINT test_run_details_archive_id_pk;
 
 ALTER TABLE toolkit.test_run_details_archive
 ADD CONSTRAINT test_run_details_archive_id_pk PRIMARY KEY (run_id, testcase_id, method_id);
-
--- add execution_status and run_status columns in test_run_archive
-ALTER TABLE toolkit.test_run_archive ADD COLUMN execution_status character varying(36) NOT NULL DEFAULT 'incomplete';
-ALTER TABLE toolkit.test_run_archive ADD COLUMN result_status character varying(36) NOT NULL DEFAULT 'failure';
-COMMENT ON COLUMN toolkit.test_run_archive.execution_status IS 'Execution Status: test run execution status Incomplete or Complete.';
-COMMENT ON COLUMN toolkit.test_run_archive.run_status IS 'Test Run Status: test run status as Failure/Success';
 
 --Script to populate the newly added columns 'execution_status', 'run_status'
 --for existing test runs 
