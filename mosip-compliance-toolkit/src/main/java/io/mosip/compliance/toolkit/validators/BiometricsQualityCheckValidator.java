24bd547e372c84dd1f621b6bfd8238aa30a70310
sudeep <sudeep.j7353@gmail.com>
2024-02-01 16:18:14 +0530

    MOSIP:31330 - iris QA report.
    Signed-off-by: sudeep <sudeep.j7353@gmail.com>

--- mosip-compliance-toolkit/src/main/java/io/mosip/compliance/toolkit/validators/BiometricsQualityCheckValidator.java
+++ mosip-compliance-toolkit/src/main/java/io/mosip/compliance/toolkit/validators/BiometricsQualityCheckValidator.java
@@ -469,8 +469,6 @@ public class BiometricsQualityCheckValidator extends ISOStandardsValidator {
 			throw e;
 		}
 		validationResultDto.setStatus(AppConstants.FAILURE);
-		validationResultDto.setDescription("SDK url [" + sdkUrl + "] is not reachable, Unable to connect.");
-		validationResultDto.setDescriptionKey("BIOMETRIC_QUALITY_CHECK_005" + AppConstants.COMMA_SEPARATOR);
 		return validationResultDto;
 	}
 }
