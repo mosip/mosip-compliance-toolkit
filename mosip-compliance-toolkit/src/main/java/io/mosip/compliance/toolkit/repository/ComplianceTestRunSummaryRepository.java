06f5ca4ec974951b29f263070f6863f2bb568667
Sudeep7353 <103890430+Sudeep7353@users.noreply.github.com>
2024-02-09 10:59:28 +0530

    MOSIP-31487 : add collection type column in My Reports and Partner Reports dashboards. (#958)
    * MOSIP-31487 : add collection type column in My Reports and Partner Reports dashboards.
    
    Signed-off-by: sudeep <sudeep.j7353@gmail.com>
    
    * MOSIP-31487 : add collection type column in My Reports and Partner Reports dashboards.
    
    Signed-off-by: sudeep <sudeep.j7353@gmail.com>
    
    * MOSIP-31487 : add collection type column in My Reports and Partner Reports dashboards.
    
    Signed-off-by: sudeep <sudeep.j7353@gmail.com>
    
    * MOSIP-31487 : add collection type column in My Reports and Partner Reports dashboards.
    
    Signed-off-by: sudeep <sudeep.j7353@gmail.com>
    
    ---------
    
    Signed-off-by: sudeep <sudeep.j7353@gmail.com>

--- mosip-compliance-toolkit/src/main/java/io/mosip/compliance/toolkit/repository/ComplianceTestRunSummaryRepository.java
+++ mosip-compliance-toolkit/src/main/java/io/mosip/compliance/toolkit/repository/ComplianceTestRunSummaryRepository.java
@@ -2,6 +2,7 @@ package io.mosip.compliance.toolkit.repository;
 
 import java.util.List;
 
+import io.mosip.compliance.toolkit.entity.ComplianceReportSummaryEntity;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -11,12 +12,57 @@ import io.mosip.kernel.core.dataaccess.spi.repository.BaseRepository;
 
 @Repository("ComplianceTestRunSummaryRepository")
 public interface ComplianceTestRunSummaryRepository
-		extends BaseRepository<ComplianceTestRunSummaryEntity, ComplianceTestRunSummaryPK> {
+        extends BaseRepository<ComplianceTestRunSummaryEntity, ComplianceTestRunSummaryPK> {
 
-	@Query("SELECT e FROM ComplianceTestRunSummaryEntity e  WHERE e.reportStatus= ?1 and e.isDeleted<>'true' order by e.crDtimes desc")
-	public List<ComplianceTestRunSummaryEntity> findAllByReportStatus(String reportStatus);
+    @Query("SELECT new io.mosip.compliance.toolkit.entity.ComplianceReportSummaryEntity(" +
+            "cts.projectId AS project_id, " +
+            "cts.collectionId AS collection_id, " +
+            "cts.runId AS run_id, " +
+            "cts.projectType AS project_type, " +
+            "cts.partnerId AS partner_id, " +
+            "cts.orgName AS org_name, " +
+            "cts.reportStatus AS report_status, " +
+            "cts.partnerComments AS partner_comments, " +
+            "cts.adminComments AS admin_comments, " +
+            "cts.reviewDtimes AS review_dttimes, " +
+            "cts.approveRejectDtimes AS approve_reject_dttimes, " +
+            "cts.crBy AS cr_by, " +
+            "cts.crDtimes AS cr_dtimes, " +
+            "cts.updBy AS upd_by, " +
+            "cts.updDtimes AS upd_dtimes, " +
+            "c.collectionType AS collection_type, " +
+            "c.name AS collection_name) " +
+            "FROM ComplianceTestRunSummaryEntity cts, CollectionEntity c " +
+            "WHERE cts.collectionId = c.id " +
+            "AND cts.reportStatus = ?1 " +
+            "AND cts.isDeleted <> 'true' " +
+            "ORDER BY cts.crDtimes DESC")
+    public List<ComplianceReportSummaryEntity> findAllByReportStatus(String reportStatus);
 
-	@Query("SELECT e FROM ComplianceTestRunSummaryEntity e  WHERE e.partnerId= ?1 and e.isDeleted<>'true' and e.reportStatus<>'draft' order by e.crDtimes desc")
-	public List<ComplianceTestRunSummaryEntity> findAllBySubmittedReportsPartnerId(String partnerId);
+    @Query("SELECT new io.mosip.compliance.toolkit.entity.ComplianceReportSummaryEntity(" +
+            "cts.projectId AS project_id, " +
+            "cts.collectionId AS collection_id, " +
+            "cts.runId AS run_id, " +
+            "cts.projectType AS project_type, " +
+            "cts.partnerId AS partner_id, " +
+            "cts.orgName AS org_name, " +
+            "cts.reportStatus AS report_status, " +
+            "cts.partnerComments AS partner_comments, " +
+            "cts.adminComments AS admin_comments, " +
+            "cts.reviewDtimes AS review_dttimes, " +
+            "cts.approveRejectDtimes AS approve_reject_dttimes, " +
+            "cts.crBy AS cr_by, " +
+            "cts.crDtimes AS cr_dtimes, " +
+            "cts.updBy AS upd_by, " +
+            "cts.updDtimes AS upd_dtimes, " +
+            "c.collectionType AS collection_type, " +
+            "c.name AS collection_name) " +
+            "FROM ComplianceTestRunSummaryEntity cts, CollectionEntity c " +
+            "WHERE cts.collectionId = c.id " +
+            "AND cts.partnerId = ?1 " +
+            "AND cts.reportStatus<>'draft' " +
+            "AND cts.isDeleted <> 'true' " +
+            "ORDER BY cts.crDtimes DESC")
+    public List<ComplianceReportSummaryEntity> findAllBySubmittedReportsPartnerId(String partnerId);
 
 }
--- mosip-compliance-toolkit/src/test/java/io/mosip/compliance/toolkit/service/ReportServiceTest.java
+++ mosip-compliance-toolkit/src/test/java/io/mosip/compliance/toolkit/service/ReportServiceTest.java
@@ -23,6 +23,7 @@ import io.mosip.compliance.toolkit.dto.report.PartnerDetailsDto.ErrorDto;
 import io.mosip.compliance.toolkit.dto.collections.CollectionTestCasesResponseDto;
 import io.mosip.compliance.toolkit.dto.report.*;
 import io.mosip.compliance.toolkit.entity.ComplianceTestRunSummaryEntity;
+import io.mosip.compliance.toolkit.entity.ComplianceReportSummaryEntity;
 import io.mosip.compliance.toolkit.entity.ComplianceTestRunSummaryPK;
 import io.mosip.compliance.toolkit.repository.*;
 import io.mosip.compliance.toolkit.util.ObjectMapperConfig;
@@ -1134,11 +1135,11 @@ public class ReportServiceTest {
     @Test(expected = Exception.class)
     public void getReportListTest() {
         boolean isAdmin = false;
-        ComplianceTestRunSummaryEntity complianceTestRunSummaryEntity = new ComplianceTestRunSummaryEntity();
-        complianceTestRunSummaryEntity.setReportStatus("teyrugt");
-        complianceTestRunSummaryEntity.setProjectType("SBI");
-        List<ComplianceTestRunSummaryEntity> listEntity = new ArrayList<>();
-        listEntity.add(complianceTestRunSummaryEntity);
+        ComplianceReportSummaryEntity complianceReportSummaryEntity = new ComplianceReportSummaryEntity();
+        complianceReportSummaryEntity.setReportStatus("teyrugt");
+        complianceReportSummaryEntity.setProjectType("SBI");
+        List<ComplianceReportSummaryEntity> listEntity = new ArrayList<>();
+        listEntity.add(complianceReportSummaryEntity);
         MosipUserDto mosipUserDto = getMosipUserDto();
         AuthUserDetails authUserDetails = new AuthUserDetails(mosipUserDto, "123");
         when(authentication.getPrincipal()).thenReturn(authUserDetails);
@@ -1153,7 +1154,7 @@ public class ReportServiceTest {
 
     @Test
     public void getProjectNameSbiTest() {
-        ComplianceTestRunSummaryEntity respEntity = new ComplianceTestRunSummaryEntity();
+        ComplianceReportSummaryEntity respEntity = new ComplianceReportSummaryEntity();
         respEntity.setReportStatus("teyrugt");
         respEntity.setProjectType("SBI");
         when(sbiProjectRepository.getProjectNameById(respEntity.getProjectId(), respEntity.getPartnerId())).thenReturn("yasud");
@@ -1163,7 +1164,7 @@ public class ReportServiceTest {
 
     @Test
     public void getProjectNameSdkTest() {
-        ComplianceTestRunSummaryEntity respEntity = new ComplianceTestRunSummaryEntity();
+        ComplianceReportSummaryEntity respEntity = new ComplianceReportSummaryEntity();
         respEntity.setReportStatus("teyrugt");
         respEntity.setProjectType("SDK");
         when(sdkProjectRepository.getProjectNameById(respEntity.getProjectId(), respEntity.getPartnerId())).thenReturn("yasud");
@@ -1173,7 +1174,7 @@ public class ReportServiceTest {
 
     @Test
     public void getProjectNameAbisTest() {
-        ComplianceTestRunSummaryEntity respEntity = new ComplianceTestRunSummaryEntity();
+        ComplianceReportSummaryEntity respEntity = new ComplianceReportSummaryEntity();
         respEntity.setReportStatus("teyrugt");
         respEntity.setProjectType("ABIS");
         when(abisProjectRepository.getProjectNameById(respEntity.getProjectId(), respEntity.getPartnerId())).thenReturn("yasud");
