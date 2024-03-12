bb62e5802bff52e9f02748a9795943cb78add8ba
Mayura Deshmukh <mayura.deshmukh@gmail.com>
2024-03-12 18:00:32 +0530

    MOSIP-24076 added batchjob tables via scripts
    Signed-off-by: Mayura Deshmukh <mayura.deshmukh@gmail.com>

--- db_upgrade_scripts/mosip_toolkit/sql/1.3.0_to_1.4.0_rollback.sql
+++ db_upgrade_scripts/mosip_toolkit/sql/1.3.0_to_1.4.0_rollback.sql
@@ -1,4 +1,19 @@
 \c mosip_toolkit sysadmin
 
+DROP SEQUENCE IF EXISTS toolkit.batch_step_execution_seq;
+DROP SEQUENCE IF EXISTS toolkit.batch_job_execution_seq;
+DROP SEQUENCE IF EXISTS toolkit.batch_job_seq;
+ALTER TABLE toolkit.batch_job_execution_params DROP CONSTRAINT job_exec_params_fk;
+ALTER TABLE toolkit.batch_job_execution_context DROP CONSTRAINT job_exec_ctx_fk;
+ALTER TABLE toolkit.batch_job_execution DROP CONSTRAINT job_inst_exec_fk;
+ALTER TABLE toolkit.batch_step_execution DROP CONSTRAINT job_exec_step_fk;
+ALTER TABLE toolkit.batch_step_execution_context DROP CONSTRAINT step_exec_ctx_fk;
+DROP TABLE IF EXISTS toolkit.batch_job_execution_params;
+DROP TABLE IF EXISTS toolkit.batch_job_execution_context;
+DROP TABLE IF EXISTS toolkit.batch_step_execution;
+DROP TABLE IF EXISTS toolkit.batch_step_execution_context;
+DROP TABLE IF EXISTS toolkit.batch_job_execution;
+DROP TABLE IF EXISTS toolkit.batch_job_instance;
+
 -- partner_profile
 DROP TABLE IF EXISTS toolkit.partner_profile;
