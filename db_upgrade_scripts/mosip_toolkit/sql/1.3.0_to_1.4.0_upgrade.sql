990ece1f5187f53e7ff529d8464d132057961f1c
sudeep <sudeep.j7353@gmail.com>
2024-03-19 16:35:10 +0530

    MOSIP:31925 - added tables for consent.
    Signed-off-by: sudeep <sudeep.j7353@gmail.com>

--- db_upgrade_scripts/mosip_toolkit/sql/1.3.0_to_1.4.0_upgrade.sql
+++ db_upgrade_scripts/mosip_toolkit/sql/1.3.0_to_1.4.0_upgrade.sql
@@ -155,6 +155,8 @@ CREATE TABLE toolkit.partner_profile(
     consent_given_dtimes timestamp NOT NULL,
     cr_dtimes timestamp NOT NULL,
     cr_by character varying(64) NOT NULL,
+    upd_by character varying(64),
+	upd_dtimes timestamp,
     CONSTRAINT partner_profile_pk PRIMARY KEY (partner_id,org_name),
     CONSTRAINT consent_given CHECK (consent_given IN ('YES', 'NO'))
 );
@@ -165,3 +167,5 @@ COMMENT ON COLUMN toolkit.partner_profile.cr_dtimes IS 'Created DateTimestamp : 
 COMMENT ON COLUMN toolkit.partner_profile.cr_by IS 'Created By : ID or name of the user who create / insert record.';
 COMMENT ON COLUMN toolkit.partner_profile.org_name IS 'Orgname: organization name to which partner belongs to.';
 COMMENT ON COLUMN toolkit.partner_profile.consent_given IS 'Consent Given : Indicates whether consent has been given by the partner.';
+COMMENT ON COLUMN toolkit.partner_profile.upd_by IS 'Updated By : ID or name of the user who update the record with new values';
+COMMENT ON COLUMN toolkit.partner_profile.upd_dtimes IS 'Updated DateTimestamp : Date and Timestamp when any of the fields in the record is updated with new values.';
