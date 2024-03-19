847acd11cfcce4365ad9d245436062381713d10e
Sudeep7353 <103890430+Sudeep7353@users.noreply.github.com>
2024-03-19 11:41:55 +0530

    MOSIP:31925 - changed template table name. (#994)
    Signed-off-by: sudeep <sudeep.j7353@gmail.com>

--- db_upgrade_scripts/mosip_toolkit/sql/1.3.0_to_1.4.0_rollback.sql
+++ db_upgrade_scripts/mosip_toolkit/sql/1.3.0_to_1.4.0_rollback.sql
@@ -19,7 +19,7 @@ DROP TABLE IF EXISTS toolkit.batch_job_instance;
 DROP TABLE IF EXISTS toolkit.partner_profile;
 
 --ctk_template
-DROP TABLE IF EXISTS toolkit.ctk_template;
+DROP TABLE IF EXISTS toolkit.custom_templates;
 
 -- decode username and password in abis projects
 UPDATE toolkit.abis_projects SET username = convert_from(decode(username, 'base64'), 'UTF8');
