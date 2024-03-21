7c2f568140793a5500a103eb36f44a4a26031ee5
sudeep <sudeep.j7353@gmail.com>
2024-03-21 13:11:37 +0530

    MOSIP:31925 - updated consent tables.
    Signed-off-by: sudeep <sudeep.j7353@gmail.com>

--- db_upgrade_scripts/mosip_toolkit/sql/1.3.0_to_1.4.0_upgrade.sql
+++ db_upgrade_scripts/mosip_toolkit/sql/1.3.0_to_1.4.0_upgrade.sql
@@ -171,167 +171,203 @@ COMMENT ON COLUMN toolkit.partner_consent.upd_by IS 'Updated By : ID or name of 
 COMMENT ON COLUMN toolkit.partner_consent.upd_dtimes IS 'Updated DateTimestamp : Date and Timestamp when any of the fields in the record is updated with new values.';
 
 --extension to create random UUID 
-
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 --insert default consent templates
-
 --english consent template
-INSERT INTO toolkit.consent_templates
-(id, lang_code, template_name, template, cr_dtimes, cr_by)
-VALUES(uuid_generate_v4(),'eng', 'terms_and_condtions_template',
-'<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-<body>
-    <h2>Terms & Conditions</h2>
-    <h3>1. Purpose of Collection:</h3>
-    <p>Biometric data will be collected solely for the purpose of testing compliance with MOSIP specifications (SDK & ABIS). This data will be used to:</p>
-    <ul>
-        <li>Verify the functionality and performance of biometric capture devices and software.</li>
-        <li>Ensure the accuracy and reliability of biometrics to do quality analysis.</li>
-    </ul>
-    <h3>2. Data Minimization:</h3>
-    <p>Only the minimum amount of biometric data necessary for testing purposes will be collected. This may include fingerprints, iris scans, or facial recognition data.</p>
-    <h3>3. Data Storage and Security:</h3>
-    <ul>
-        <li>Biometric data will be stored securely and encrypted within the CTK platform.</li>
-        <li>Access to this data will be restricted to authorized personnel only.</li>
-        <li>Appropriate technical and organizational safeguards will be implemented to protect against unauthorized access, disclosure, alteration, or destruction of the data.</li>
-    </ul>
-    <h3>4. Data Retention and Deletion:</h3>
-    <ul>
-        <li>Biometric data will be retained until the user’s project is active in the CTK platform.</li>
-        <li>Users will have the option to request the deletion of their data at any time.</li>
-    </ul>
-    <h3>5. Non-Sharing of Data:</h3>
-    <ul>
-        <li>Biometric data collected within CTK will not be shared with third-party vendors or individuals outside the authorized CTK personnel.</li>
-        <li>The data will not be used for any purpose besides CTK compliance testing.</li>
-        <li>Biometric data will not be linked or mapped to any specific individual.</li>
-    </ul>
-    <h3>6. User Rights:</h3>
-    <p>Users have the right to:</p>
-    <ul>
-        <li>Access their biometric data stored within CTK.</li>
-        <li>Request correction of any inaccurate data.</li>
-        <li>Withdraw their consent for data collection at any time.</li>
-        <li>Lodge a complaint with the appropriate data protection authority if they have concerns about how their data is being handled.</li>
-    </ul>
-    <h3>7. Governing Law:</h3>
-    <p>This consent is subject to the laws and regulations of the jurisdiction where the data is collected.</p>
-    <h3>8. Updates to Terms and Conditions:</h3>
-    <p>We reserve the right to update these terms and conditions at any time. Any changes will be communicated to users through the CTK platform.</p>
-</body>
-</html>'
-, current_timestamp, 'admin');
+INSERT INTO
+    toolkit.consent_templates (
+        id,
+        lang_code,
+        template_name,
+        template,
+        cr_dtimes,
+        cr_by
+    )
+VALUES
+    (
+        uuid_generate_v4(),
+        'eng',
+        'terms_and_condtions_template',
+        '<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        </head>
+        <body>
+            <h2>Terms & Conditions</h2>
+            <h3>1. Purpose of Collection:</h3>
+            <p>Biometric data will be collected solely for the purpose of testing compliance with MOSIP specifications (SDK & ABIS). This data will be used to:</p>
+            <ul>
+                <li>Verify the functionality and performance of biometric capture devices and software.</li>
+                <li>Ensure the accuracy and reliability of biometrics to do quality analysis.</li>
+            </ul>
+            <h3>2. Data Minimization:</h3>
+            <p>Only the minimum amount of biometric data necessary for testing purposes will be collected. This may include fingerprints, iris scans, or facial recognition data.</p>
+            <h3>3. Data Storage and Security:</h3>
+            <ul>
+                <li>Biometric data will be stored securely and encrypted within the CTK platform.</li>
+                <li>Access to this data will be restricted to authorized personnel only.</li>
+                <li>Appropriate technical and organizational safeguards will be implemented to protect against unauthorized access, disclosure, alteration, or destruction of the data.</li>
+            </ul>
+            <h3>4. Data Retention and Deletion:</h3>
+            <ul>
+                <li>Biometric data will be retained until the user’s project is active in the CTK platform.</li>
+                <li>Users will have the option to request the deletion of their data at any time.</li>
+            </ul>
+            <h3>5. Non-Sharing of Data:</h3>
+            <ul>
+                <li>Biometric data collected within CTK will not be shared with third-party vendors or individuals outside the authorized CTK personnel.</li>
+                <li>The data will not be used for any purpose besides CTK compliance testing.</li>
+                <li>Biometric data will not be linked or mapped to any specific individual.</li>
+            </ul>
+            <h3>6. User Rights:</h3>
+            <p>Users have the right to:</p>
+            <ul>
+                <li>Access their biometric data stored within CTK.</li>
+                <li>Request correction of any inaccurate data.</li>
+                <li>Withdraw their consent for data collection at any time.</li>
+                <li>Lodge a complaint with the appropriate data protection authority if they have concerns about how their data is being handled.</li>
+            </ul>
+            <h3>7. Governing Law:</h3>
+            <p>This consent is subject to the laws and regulations of the jurisdiction where the data is collected.</p>
+            <h3>8. Updates to Terms and Conditions:</h3>
+            <p>We reserve the right to update these terms and conditions at any time. Any changes will be communicated to users through the CTK platform.</p>
+        </body>
+        </html>',
+        current_timestamp,
+        'admin'
+    );
 
 --arabic consent template
-INSERT INTO toolkit.consent_templates
-(id, lang_code, template_name, template, cr_dtimes, cr_by)
-VALUES(uuid_generate_v4(),'ara', 'terms_and_condtions_template',
-'<!DOCTYPE html>
-<html lang="ar">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-<body>
-    <h2>البنود و الظروف</h2>
-    <h3>1. الغرض من الجمع:</h3>
-    <p>سيتم جمع البيانات البيومترية فقط لغرض اختبار الامتثال لمواصفات MOSIP (SDK & ABIS). سيتم استخدام هذه البيانات من أجل:</p>
-    <ul>
-        <li>التحقق من وظائف وأداء أجهزة وبرامج التقاط القياسات الحيوية.</li>
-        <li>التأكد من دقة وموثوقية القياسات الحيوية لإجراء تحليل الجودة.</li>
-    </ul>
-    <h3>2. تقليل البيانات:</h3>
-    <p>سيتم جمع الحد الأدنى فقط من البيانات البيومترية اللازمة لأغراض الاختبار. قد يشمل ذلك بصمات الأصابع أو مسح قزحية العين أو بيانات التعرف على الوجه.</p>
-    <h3>3. تخزين البيانات وأمنها:</h3>
-    <ul>
-        <li>سيتم تخزين البيانات البيومترية بشكل آمن ومشفرة داخل منصة CTK.</li>
-        <li>يقتصر الوصول إلى هذه البيانات على الموظفين المصرح لهم فقط.</li>
-        <li>سيتم تنفيذ الضمانات الفنية والتنظيمية المناسبة للحماية من الوصول غير المصرح به للبيانات أو الكشف عنها أو تغييرها أو تدميرها.</li>
-    </ul>
-    <h3>4. الاحتفاظ بالبيانات وحذفها:</h3>
-    <ul>
-        <li>سيتم الاحتفاظ بالبيانات البيومترية حتى يصبح مشروع المستخدم نشطًا في منصة CTK.</li>
-        <li>سيكون لدى المستخدمين خيار طلب حذف بياناتهم في أي وقت.</li>
-    </ul>
-    <h3>5. عدم مشاركة البيانات:</h3>
-    <ul>
-        <li>لن تتم مشاركة البيانات البيومترية التي تم جمعها داخل CTK مع بائعي الطرف الثالث أو الأفراد خارج موظفي CTK المعتمدين.</li>
-        <li>لن يتم استخدام البيانات لأي غرض غير اختبار الامتثال لـ CTK.</li>
-        <li>لن يتم ربط البيانات البيومترية أو تعيينها لأي فرد محدد.</li>
-    </ul>
-    <h3>6. حقوق المستخدم:</h3>
-    <p>للمستخدمين الحق في:</p>
-    <ul>
-        <li>الوصول إلى بياناتهم البيومترية المخزنة داخل CTK.</li>
-        <li>طلب تصحيح أي بيانات غير دقيقة.</li>
-        <li>سحب موافقتهم على جمع البيانات في أي وقت.</li>
-        <li>قم بتقديم شكوى إلى هيئة حماية البيانات المناسبة إذا كانت لديهم مخاوف بشأن كيفية التعامل مع بياناتهم.</li>
-    </ul>
-    <h3>7. القانون الحاكم:</h3>
-    <p>تخضع هذه الموافقة لقوانين ولوائح الولاية القضائية التي يتم فيها جمع البيانات.</p>
-    <h3>8. تحديثات الشروط والأحكام:</h3>
-    <p>نحن نحتفظ بالحق في تحديث هذه الشروط والأحكام في أي وقت. سيتم إرسال أي تغييرات إلى المستخدمين من خلال منصة CTK.</p>
-</body>
-</html>'
-, current_timestamp, 'admin');
+INSERT INTO
+    toolkit.consent_templates (
+        id,
+        lang_code,
+        template_name,
+        template,
+        cr_dtimes,
+        cr_by
+    )
+VALUES
+    (
+        uuid_generate_v4(),
+        'ara',
+        'terms_and_condtions_template',
+        '<!DOCTYPE html>
+        <html lang="ar">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        </head>
+        <body>
+            <h2>البنود و الظروف</h2>
+            <h3>1. الغرض من الجمع:</h3>
+            <p>سيتم جمع البيانات البيومترية فقط لغرض اختبار الامتثال لمواصفات MOSIP (SDK & ABIS). سيتم استخدام هذه البيانات من أجل:</p>
+            <ul>
+                <li>التحقق من وظائف وأداء أجهزة وبرامج التقاط القياسات الحيوية.</li>
+                <li>التأكد من دقة وموثوقية القياسات الحيوية لإجراء تحليل الجودة.</li>
+            </ul>
+            <h3>2. تقليل البيانات:</h3>
+            <p>سيتم جمع الحد الأدنى فقط من البيانات البيومترية اللازمة لأغراض الاختبار. قد يشمل ذلك بصمات الأصابع أو مسح قزحية العين أو بيانات التعرف على الوجه.</p>
+            <h3>3. تخزين البيانات وأمنها:</h3>
+            <ul>
+                <li>سيتم تخزين البيانات البيومترية بشكل آمن ومشفرة داخل منصة CTK.</li>
+                <li>يقتصر الوصول إلى هذه البيانات على الموظفين المصرح لهم فقط.</li>
+                <li>سيتم تنفيذ الضمانات الفنية والتنظيمية المناسبة للحماية من الوصول غير المصرح به للبيانات أو الكشف عنها أو تغييرها أو تدميرها.</li>
+            </ul>
+            <h3>4. الاحتفاظ بالبيانات وحذفها:</h3>
+            <ul>
+                <li>سيتم الاحتفاظ بالبيانات البيومترية حتى يصبح مشروع المستخدم نشطًا في منصة CTK.</li>
+                <li>سيكون لدى المستخدمين خيار طلب حذف بياناتهم في أي وقت.</li>
+            </ul>
+            <h3>5. عدم مشاركة البيانات:</h3>
+            <ul>
+                <li>لن تتم مشاركة البيانات البيومترية التي تم جمعها داخل CTK مع بائعي الطرف الثالث أو الأفراد خارج موظفي CTK المعتمدين.</li>
+                <li>لن يتم استخدام البيانات لأي غرض غير اختبار الامتثال لـ CTK.</li>
+                <li>لن يتم ربط البيانات البيومترية أو تعيينها لأي فرد محدد.</li>
+            </ul>
+            <h3>6. حقوق المستخدم:</h3>
+            <p>للمستخدمين الحق في:</p>
+            <ul>
+                <li>الوصول إلى بياناتهم البيومترية المخزنة داخل CTK.</li>
+                <li>طلب تصحيح أي بيانات غير دقيقة.</li>
+                <li>سحب موافقتهم على جمع البيانات في أي وقت.</li>
+                <li>قم بتقديم شكوى إلى هيئة حماية البيانات المناسبة إذا كانت لديهم مخاوف بشأن كيفية التعامل مع بياناتهم.</li>
+            </ul>
+            <h3>7. القانون الحاكم:</h3>
+            <p>تخضع هذه الموافقة لقوانين ولوائح الولاية القضائية التي يتم فيها جمع البيانات.</p>
+            <h3>8. تحديثات الشروط والأحكام:</h3>
+            <p>نحن نحتفظ بالحق في تحديث هذه الشروط والأحكام في أي وقت. سيتم إرسال أي تغييرات إلى المستخدمين من خلال منصة CTK.</p>
+        </body>
+        </html>',
+        current_timestamp,
+        'admin'
+    );
 
 --french consent template 
-
-INSERT INTO toolkit.consent_templates
-(id, lang_code, template_name, template, cr_dtimes, cr_by)
-VALUES(uuid_generate_v4(),'fra', 'terms_and_condtions_template',
-'<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-<body>
-    <h2>termes et conditions</h2>
-    <h3>1. Objet de la collecte:</h3>
-    <p>Les données biométriques seront collectées uniquement dans le but de tester la conformité aux spécifications MOSIP (SDK & ABIS). Ces données seront utilisées pour :</p>
-    <ul>
-        <li>Vérifier la fonctionnalité et les performances des dispositifs et logiciels de capture biométrique.</li>
-        <li>Assurer l’exactitude et la fiabilité des données biométriques pour effectuer des analyses de qualité.</li>
-    </ul>
-    <h3>2. Minimisation des données:</h3>
-    <p>Seule la quantité minimale de données biométriques nécessaire aux fins des tests sera collectée. Cela peut inclure des empreintes digitales, des scans de l iris ou des données de reconnaissance faciale.</p>
-    <h3>3. Stockage et sécurité des données:</h3>
-    <ul>
-        <li>Les données biométriques seront stockées en toute sécurité et cryptées au sein de la plateforme CTK.</li>
-        <li>L accès à ces données sera limité au personnel autorisé uniquement.</li>
-        <li>Des mesures de protection techniques et organisationnelles appropriées seront mises en œuvre pour protéger contre l accès non autorisé, la divulgation, l altération ou la destruction des données.</li>
-    </ul>
-    <h3>4. Conservation et suppression des données:</h3>
-    <ul>
-        <li>Les données biométriques seront conservées jusqu’à ce que le projet de l’utilisateur soit actif dans la plateforme CTK.</li>
-        <li>Les utilisateurs auront la possibilité de demander la suppression de leurs données à tout moment.</li>
-    </ul>
-    <h3>5. Non-partage de données:</h3>
-    <ul>
-        <li>Les données biométriques collectées au sein de CTK ne seront pas partagées avec des fournisseurs tiers ou des personnes extérieures au personnel autorisé de CTK.</li>
-        <li>Les données ne seront utilisées à aucune fin autre que les tests de conformité CTK.</li>
-        <li>Biometric data will not be linked or mapped to any specific individual.</li>
-    </ul>
-    <h3>6. Droits des utilisateurs:</h3>
-    <p>Les utilisateurs ont le droit de:</p>
-    <ul>
-        <li>Accédez à leurs données biométriques stockées dans CTK.</li>
-        <li>Demander la correction de toute donnée inexacte.</li>
-        <li>Retirer à tout moment son consentement à la collecte de données.</li>
-        <li>Déposez une plainte auprès de l autorité de protection des données compétente s ils ont des inquiétudes quant à la manière dont leurs données sont traitées.</li>
-    </ul>
-    <h3>7. Loi applicable:</h3>
-    <p>Ce consentement est soumis aux lois et réglementations de la juridiction où les données sont collectées.</p>
-    <h3>8. Mises à jour des termes et conditions:</h3>
-    <p>Nous nous réservons le droit de mettre à jour ces termes et conditions à tout moment. Toute modification sera communiquée aux utilisateurs via la plateforme CTK.</p>
-</body>
-</html>'
-, current_timestamp, 'admin');
+INSERT INTO
+    toolkit.consent_templates (
+        id,
+        lang_code,
+        template_name,
+        template,
+        cr_dtimes,
+        cr_by
+    )
+VALUES
+    (
+        uuid_generate_v4(),
+        'fra',
+        'terms_and_condtions_template',
+        '<!DOCTYPE html>
+        <html lang="fr">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        </head>
+        <body>
+            <h2>termes et conditions</h2>
+            <h3>1. Objet de la collecte:</h3>
+            <p>Les données biométriques seront collectées uniquement dans le but de tester la conformité aux spécifications MOSIP (SDK & ABIS). Ces données seront utilisées pour :</p>
+            <ul>
+                <li>Vérifier la fonctionnalité et les performances des dispositifs et logiciels de capture biométrique.</li>
+                <li>Assurer l’exactitude et la fiabilité des données biométriques pour effectuer des analyses de qualité.</li>
+            </ul>
+            <h3>2. Minimisation des données:</h3>
+            <p>Seule la quantité minimale de données biométriques nécessaire aux fins des tests sera collectée. Cela peut inclure des empreintes digitales, des scans de l iris ou des données de reconnaissance faciale.</p>
+            <h3>3. Stockage et sécurité des données:</h3>
+            <ul>
+                <li>Les données biométriques seront stockées en toute sécurité et cryptées au sein de la plateforme CTK.</li>
+                <li>L accès à ces données sera limité au personnel autorisé uniquement.</li>
+                <li>Des mesures de protection techniques et organisationnelles appropriées seront mises en œuvre pour protéger contre l accès non autorisé, la divulgation, l altération ou la destruction des données.</li>
+            </ul>
+            <h3>4. Conservation et suppression des données:</h3>
+            <ul>
+                <li>Les données biométriques seront conservées jusqu’à ce que le projet de l’utilisateur soit actif dans la plateforme CTK.</li>
+                <li>Les utilisateurs auront la possibilité de demander la suppression de leurs données à tout moment.</li>
+            </ul>
+            <h3>5. Non-partage de données:</h3>
+            <ul>
+                <li>Les données biométriques collectées au sein de CTK ne seront pas partagées avec des fournisseurs tiers ou des personnes extérieures au personnel autorisé de CTK.</li>
+                <li>Les données ne seront utilisées à aucune fin autre que les tests de conformité CTK.</li>
+                <li>Biometric data will not be linked or mapped to any specific individual.</li>
+            </ul>
+            <h3>6. Droits des utilisateurs:</h3>
+            <p>Les utilisateurs ont le droit de:</p>
+            <ul>
+                <li>Accédez à leurs données biométriques stockées dans CTK.</li>
+                <li>Demander la correction de toute donnée inexacte.</li>
+                <li>Retirer à tout moment son consentement à la collecte de données.</li>
+                <li>Déposez une plainte auprès de l autorité de protection des données compétente s ils ont des inquiétudes quant à la manière dont leurs données sont traitées.</li>
+            </ul>
+            <h3>7. Loi applicable:</h3>
+            <p>Ce consentement est soumis aux lois et réglementations de la juridiction où les données sont collectées.</p>
+            <h3>8. Mises à jour des termes et conditions:</h3>
+            <p>Nous nous réservons le droit de mettre à jour ces termes et conditions à tout moment. Toute modification sera communiquée aux utilisateurs via la plateforme CTK.</p>
+        </body>
+        </html>',
+        current_timestamp,
+        'admin'
+    );
