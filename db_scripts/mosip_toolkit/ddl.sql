7fcf1b3e2c01c7cb865f098d60d51194c60dfdde
sudeep <sudeep.j7353@gmail.com>
2024-03-21 15:04:53 +0530

    MOSIP:31925 - updated consent tables.
    Signed-off-by: sudeep <sudeep.j7353@gmail.com>

--- db_scripts/mosip_toolkit/ddl.sql
+++ db_scripts/mosip_toolkit/ddl.sql
@@ -22,5 +22,5 @@
 \ir ddl/batch_step_execution.sql
 \ir ddl/batch_step_execution_context.sql
 \ir ddl/batch-fk.sql
-\ir ddl/custom_templates.sql
-\ir ddl/partner_profile.sql
+\ir ddl/consent_templates.sql
+\ir ddl/partner_consent.sql
